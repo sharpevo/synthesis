5d22fcc76db0fa34fabebcaf789c666270e85eb6
sharpevo <sharpevo@outlook.com>
2018-07-13 13:15:39 +0800

    Add response checking

--- alientek/dao.go
+++ alientek/dao.go
@@ -1,6 +1,7 @@
 package alientek
 
 import (
+	"fmt"
 	"log"
 	"posam/dao"
 	"posam/protocol/serialport"
@@ -11,23 +12,55 @@ type Dao struct {
 	SerialPort    serialport.SerialPorter
 }
 
-func (d *Dao) TurnOnLed() {
+func (d *Dao) TurnOnLed() (resp string, err error) {
 	req := d.makeRequest(LedOnUnit.Request())
-	err := d.SerialPort.Send(req.Bytes())
+	err = d.SerialPort.Send(req.Bytes())
 	if err != nil {
 		log.Println(err)
+		return resp, err
 	}
+
+	resp, err = d.checkResponse("Turn on LED", "sent", LedOnUnit.RecResp())
+	if err != nil {
+		return
+	}
+	resp, err = d.checkResponse("Turn on LED", "complete", LedOnUnit.ComResp())
+	if err != nil {
+		return
+	}
+
+	return
 }
 
-func (d *Dao) TurnOffLed() {
+func (d *Dao) TurnOffLed() (resp string, err error) {
 	req := d.makeRequest(LedOffUnit.Request())
-	err := d.SerialPort.Send(req.Bytes())
+	err = d.SerialPort.Send(req.Bytes())
 	if err != nil {
 		log.Println(err)
 	}
+
+	resp, err = d.checkResponse("Turn off LED", "sent", LedOffUnit.RecResp())
+	if err != nil {
+		return
+	}
+	resp, err = d.checkResponse("Turn off LED", "complete", LedOffUnit.ComResp())
+	if err != nil {
+		return
+	}
+	return
 }
 
 func (d *Dao) makeRequest(req dao.Request) dao.Request {
 	req.Address = d.DeviceAddress
 	return req
+}
+
+func (d *Dao) checkResponse(title string, action string, expect []byte) (resp string, err error) {
+	log.Printf("%s: check %s response %x", title, action, expect)
+	respBytes, err := d.SerialPort.Receive(expect)
+	resp = fmt.Sprintf("%x", respBytes)
+	if err != nil {
+		return resp, fmt.Errorf("%s: failed to %s | %s", title, action, err)
+	}
+	return resp, nil
 }
