42fdfbdf432a433f74346feb4f597ab73e163e45
yangwu <yang.wu@igenetech.com>
2018-08-01 15:35:15 +0800

    Add porter interface to mock OpenPort in test case
    Issue: #323

--- serialport/serialport.go
+++ serialport/serialport.go
@@ -8,15 +8,36 @@ import (
 	"posam/protocol/modbus"
 )
 
+type Porter interface {
+	OpenPort(string, int) (*serial.Port, error)
+}
+
 type SerialPorter interface {
 	Instance() *serial.Port
 	Send([]byte) error
 	Receive([]byte) ([]byte, error)
 }
 
-var instanceMap map[string]*serial.Port
+type Port struct {
+}
+
+func (p *Port) OpenPort(name string, baud int) (*serial.Port, error) {
+	log.Printf("Opening serial port %q...", name)
+	c := &serial.Config{
+		Name: name,
+		Baud: baud,
+	}
+	openedPort, err := serial.OpenPort(c)
+	if err != nil {
+		log.Println(err)
+		return nil, err
+	}
+	return openedPort, nil
+}
+
 
 type SerialPort struct {
+	Porter
 	Name     string
 	BaudRate int
 
@@ -37,16 +58,8 @@ func (s *SerialPort) Instance() *serial.Port {
 	if instanceMap[s.Name] != nil {
 		return instanceMap[s.Name]
 	}
-	log.Printf("Opening serial port %q...", s.Name)
-	c := &serial.Config{
-		Name: s.Name,
-		Baud: s.BaudRate,
 	}
-	p, err := serial.OpenPort(c)
-	if err != nil {
-		log.Println(err)
-		return nil
-	}
+	p, _ := s.OpenPort(s.Name, s.BaudRate)
 	instanceMap[s.Name] = p
 	return instanceMap[s.Name]
 
