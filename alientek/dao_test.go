97afbfffae788f480f8c3231005c4a935e9d5871
yangwu <yang.wu@igenetech.com>
2018-08-01 08:38:44 +0800

    Add Instance Operation testing

--- alientek/dao_test.go
+++ alientek/dao_test.go
@@ -4,6 +4,7 @@ import (
 	"log"
 	"posam/dao/alientek"
 	"posam/protocol/serialport"
+	"reflect"
 	"testing"
 )
 
@@ -38,4 +39,27 @@ func TestTurnOnLed(t *testing.T) {
 
 	alientekDao.TurnOnLed()
 
+}
+
+func TestInstanceOperation(t *testing.T) {
+
+	alientekDao := &alientek.Dao{
+		DeviceAddress: 0x01,
+		SerialPort: &MockSerialPort{
+			serialport.SerialPort{
+				Name:     "/dev/ttyUSB0",
+				BaudRate: 9600,
+				DataBits: 8,
+				StopBits: 1,
+				Parity:   -1,
+			},
+		},
+	}
+	alientek.AddInstance(alientekDao)
+	if !reflect.DeepEqual(
+		alientekDao,
+		alientek.Instance(string(alientekDao.DeviceAddress)),
+	) {
+		t.Errorf("Failed to get instance from deviceMap")
+	}
 }
