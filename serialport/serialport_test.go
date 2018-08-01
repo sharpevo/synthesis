794bc568150a96c39505d5d068ffb788bb1f5c48
yangwu <yang.wu@igenetech.com>
2018-08-01 15:37:12 +0800

    Add test case for map operations
    Issue: #323

---
+++ serialport/serialport_test.go
@@ -0,0 +1,77 @@
+package serialport_test
+
+import (
+	"github.com/tarm/serial"
+	"posam/protocol/serialport"
+
+	"testing"
+)
+
+type MockPort struct {
+	serialport.Port
+}
+
+func (p *MockPort) OpenPort(name string, baud int) (*serial.Port, error) {
+	return &serial.Port{}, nil
+}
+
+func TestInstanceOperationOnMap(t *testing.T) {
+	sp1 := &serialport.SerialPort{
+		&MockPort{
+			Port: serialport.Port{},
+		},
+		"/dev/ttyUSB0",
+		9601,
+		0x01,
+		8,
+		1,
+		-1,
+	}
+	instance1 := sp1.Instance()
+
+	sp2 := &serialport.SerialPort{
+		&MockPort{
+			Port: serialport.Port{},
+		},
+		"/dev/ttyUSB1",
+		9602,
+		0x01,
+		8,
+		1,
+		-1,
+	}
+	instance2 := sp2.Instance()
+
+	sp3 := &serialport.SerialPort{
+		&MockPort{
+			Port: serialport.Port{},
+		},
+		"/dev/ttyUSB0",
+		9603,
+		0x01,
+		8,
+		1,
+		-1,
+	}
+	instance3 := sp3.Instance()
+	t.Logf(
+		"%p : %p : %p\n",
+		instance1,
+		instance2,
+		instance3,
+	)
+	if instance1 == instance2 {
+		t.Errorf(
+			"Same serial port for the different devices\nEXPECT: %p\nGET: %p\n\n",
+			instance1,
+			instance2,
+		)
+	}
+	if instance1 != instance3 {
+		t.Errorf(
+			"Different serial port for the same devices\nEXPECT: %p\nGET: %p\n\n",
+			instance1,
+			instance3,
+		)
+	}
+}
