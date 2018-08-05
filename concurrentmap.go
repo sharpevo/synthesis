43840949f88c3de50d3ecaee7a5fb925ac32c359
yangwu <yang.wu@igenetech.com>
2018-08-05 15:53:20 +0800

    Add Iter function to range the items in concurrent map
    Issue: #319

--- concurrentmap.go
+++ concurrentmap.go
@@ -9,6 +9,10 @@ type ConcurrentMap struct {
 	m map[string]interface{}
 }
 
+type Item struct {
+	Key   string
+	Value interface{}
+}
 func NewConcurrentMap() *ConcurrentMap {
 	return &ConcurrentMap{
 		m: make(map[string]interface{}),
@@ -27,4 +31,17 @@ func (c *ConcurrentMap) Set(key string, value interface{}) interface{} {
 	defer c.Unlock()
 	c.m[key] = value
 	return value
+}
+
+func (c *ConcurrentMap) Iter() <-chan Item {
+	itemc := make(chan Item)
+	go func() {
+		defer close(itemc)
+		c.Lock() // more secure than RLock
+		defer c.Unlock()
+		for k, v := range c.m {
+			itemc <- Item{k, v}
+		}
+	}()
+	return itemc
 }
