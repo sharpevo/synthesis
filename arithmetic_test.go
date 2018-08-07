c6ba31952c79f3ce65a8e4c417378c1a09c78a02
yangwu <yang.wu@igenetech.com>
2018-08-07 13:55:32 +0800

    Add ParseObjects test case #318

--- arithmetic_test.go
+++ arithmetic_test.go
@@ -1,12 +1,76 @@
 package instruction_test
 
 import (
+	"fmt"
 	"math/big"
 	"posam/instruction"
 	"posam/interpreter"
 	"posam/util/concurrentmap"
+	"strings"
 	"testing"
 )
+
+func TestParseObjects(t *testing.T) {
+	var variable *interpreter.Variable
+	var v1 *big.Float
+	var v2 *big.Float
+	var err error
+
+	i := instruction.InstructionArithmetic{}
+	i.Env = concurrentmap.NewConcurrentMap()
+	i.Env.Set("var1", &interpreter.Variable{Value: "11.11"})
+	i.Env.Set("var2", &interpreter.Variable{Value: "22.22"})
+	i.Env.Set("var3", interpreter.Variable{Value: "not variable pointer"})
+
+	variable, v1, v2, err = i.ParseObjects("var1", "var2")
+	if v1.Cmp(big.NewFloat(11.11)) != 0 ||
+		v2.Cmp(big.NewFloat(22.22)) != 0 {
+		t.Errorf(
+			"\nEXPECT: %v\nGET: %v\n",
+			"11.11 and 22.22",
+			fmt.Sprintf("%s and %s", v1.String(), v2.String()),
+		)
+	}
+
+	variable, v1, v2, err = i.ParseObjects("var1", "33.33")
+	if v1.Cmp(big.NewFloat(11.11)) != 0 ||
+		v2.Cmp(big.NewFloat(33.33)) != 0 {
+		t.Errorf(
+			"\nEXPECT: %v\nGET: %v\n",
+			"11.11 and 33.33",
+			fmt.Sprintf("%s and %s", v1.String(), v2.String()),
+		)
+	}
+
+	variable, v1, v2, err = i.ParseObjects("var1", "var3")
+	if !strings.Contains(err.Error(), "invalid float") {
+		t.Errorf(
+			"\nEXPECT: %v\nGET: %v\n",
+			"invalid float",
+			err.Error(),
+		)
+	}
+
+	variable, v1, v2, err = i.ParseObjects("var3", "33.33")
+	if !strings.Contains(err.Error(), "Invalid type of variable") {
+		t.Errorf(
+			"\nEXPECT: %v\nGET: %v\n",
+			"Invalid type of variable",
+			err.Error(),
+		)
+	}
+
+	variable, v1, v2, err = i.ParseObjects("invalid", "33.33")
+	if !strings.Contains(err.Error(), "Invalid variable") {
+		t.Errorf(
+			"\nEXPECT: %v\nGET: %v\n",
+			"Invalid variable",
+			err.Error(),
+		)
+	}
+	fmt.Println(variable)
+
+}
 
 func TestGetBigFloat64(t *testing.T) {
 	var v *big.Float
