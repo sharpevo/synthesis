81b959e492cb85611c80e6dbcca397479f7bf993
yangwu <yang.wu@igenetech.com>
2018-08-07 13:39:22 +0800

    Use ParseObjects #318

--- arithmetic.sub.float64.go
+++ arithmetic.sub.float64.go
@@ -1,35 +1,17 @@
 package instruction
 
-import (
-	"fmt"
-	"posam/interpreter"
-)
+import ()
 
 type InstructionSubtractionFloat64 struct {
 	InstructionArithmetic
 }
 
 func (i *InstructionSubtractionFloat64) Execute(args ...string) (resp interface{}, err error) {
-	v, found := i.Env.Get(args[0])
-	if !found {
-		return resp, fmt.Errorf("Invalid variable %q", args[0])
-	}
-	variable, ok := v.(*interpreter.Variable)
-	if !ok {
-		return resp, fmt.Errorf("Invalid type of variable %q", args[0])
-	}
-
-	v1, err := i.GetBigFloat64(args[0])
-	if err != nil {
-		return resp, err
-	}
-
-	v2, err := i.GetBigFloat64(args[1])
+	variable, v1, v2, err := i.ParseObjects(args[0], args[1])
 	if err != nil {
 		return resp, err
 	}
 	v1.Sub(v1, v2)
-
 	variable.Value = v1.String()
 	return v1.String(), nil
 }
