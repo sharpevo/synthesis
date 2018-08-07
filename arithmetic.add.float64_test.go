8ff40be2f28eb51b90532bb3a99f0f10bed77b9b
yangwu <yang.wu@igenetech.com>
2018-08-07 13:56:05 +0800

    Remove parsing tests in add and sub instruction #318

--- arithmetic.add.float64_test.go
+++ arithmetic.add.float64_test.go
@@ -4,19 +4,16 @@ import (
 	"posam/instruction"
 	"posam/interpreter"
 	"posam/util/concurrentmap"
-	"strings"
 	"testing"
 )
 
 func TestInstructionAdditionFloat64Execute(t *testing.T) {
-	var err error
 	i := instruction.InstructionAdditionFloat64{}
 	i.Env = concurrentmap.NewConcurrentMap()
 	i.Env.Set("var1", &interpreter.Variable{Value: "12.34"})
 	i.Env.Set("var2", &interpreter.Variable{Value: "43.21"})
 
-	// addition of float variable
-	_, err = i.Execute("var1", "var2")
+	i.Execute("var1", "var2")
 	if v, _ := i.Env.Get("var1"); v.(*interpreter.Variable).Value != "55.55" {
 		t.Errorf(
 			"\nEXPECT: %v\nGET: %v\n",
@@ -25,34 +22,12 @@ func TestInstructionAdditionFloat64Execute(t *testing.T) {
 		)
 	}
 
-	// addition of variable and float
-	_, err = i.Execute("var1", "33.33")
+	i.Execute("var1", "33.33")
 	if v, _ := i.Env.Get("var1"); v.(*interpreter.Variable).Value != "88.88" {
 		t.Errorf(
 			"\nEXPECT: %v\nGET: %v\n",
 			"88.88",
 			v.(*interpreter.Variable).Value,
-		)
-	}
-
-	// addition of invalid variable
-	_, err = i.Execute("invalid", "33.33")
-	if !strings.Contains(err.Error(), "Invalid variable") {
-		t.Errorf(
-			"\nEXPECT: %v\nGET: %v\n",
-			"Invalid variable",
-			err.Error(),
-		)
-	}
-
-	// addition of variable that is not Variable pointer
-	i.Env.Set("var3", interpreter.Variable{Value: "not variable pointer"})
-	_, err = i.Execute("var3", "33.33")
-	if !strings.Contains(err.Error(), "Invalid type of variable") {
-		t.Errorf(
-			"\nEXPECT: %v\nGET: %v\n",
-			"Invalid variable",
-			err.Error(),
 		)
 	}
 }
--- arithmetic.sub.float64_test.go
+++ arithmetic.sub.float64_test.go
@@ -4,19 +4,16 @@ import (
 	"posam/instruction"
 	"posam/interpreter"
 	"posam/util/concurrentmap"
-	"strings"
 	"testing"
 )
 
 func TestInstructionSubtractionFloat64Execute(t *testing.T) {
-	var err error
 	i := instruction.InstructionSubtractionFloat64{}
 	i.Env = concurrentmap.NewConcurrentMap()
 	i.Env.Set("var1", &interpreter.Variable{Value: "88.88"})
 	i.Env.Set("var2", &interpreter.Variable{Value: "11.11"})
 
-	// addition of float variable
-	_, err = i.Execute("var1", "var2")
+	i.Execute("var1", "var2")
 	if v, _ := i.Env.Get("var1"); v.(*interpreter.Variable).Value != "77.77" {
 		t.Errorf(
 			"\nEXPECT: %v\nGET: %v\n",
@@ -25,34 +22,12 @@ func TestInstructionSubtractionFloat64Execute(t *testing.T) {
 		)
 	}
 
-	// addition of variable and float
-	_, err = i.Execute("var1", "22.22")
+	i.Execute("var1", "22.22")
 	if v, _ := i.Env.Get("var1"); v.(*interpreter.Variable).Value != "55.55" {
 		t.Errorf(
 			"\nEXPECT: %v\nGET: %v\n",
 			"55.55",
 			v.(*interpreter.Variable).Value,
-		)
-	}
-
-	// addition of invalid variable
-	_, err = i.Execute("invalid", "33.33")
-	if !strings.Contains(err.Error(), "Invalid variable") {
-		t.Errorf(
-			"\nEXPECT: %v\nGET: %v\n",
-			"Invalid variable",
-			err.Error(),
-		)
-	}
-
-	// addition of variable that is not Variable pointer
-	i.Env.Set("var3", interpreter.Variable{Value: "not variable pointer"})
-	_, err = i.Execute("var3", "33.33")
-	if !strings.Contains(err.Error(), "Invalid type of variable") {
-		t.Errorf(
-			"\nEXPECT: %v\nGET: %v\n",
-			"Invalid variable",
-			err.Error(),
 		)
 	}
 }
