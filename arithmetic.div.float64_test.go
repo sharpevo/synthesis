183993afeb76f47f62291760d6d15abf7afc8f32
yangwu <yang.wu@igenetech.com>
2018-08-07 14:13:59 +0800

    Add InstructionDivisionFloat64 and test case #318

---
+++ arithmetic.div.float64_test.go
@@ -0,0 +1,54 @@
+package instruction_test
+
+import (
+	"posam/instruction"
+	"posam/interpreter"
+	"posam/util/concurrentmap"
+	"strings"
+	"testing"
+)
+
+func TestInstructionAdditionFloat64Execute(t *testing.T) {
+	i := instruction.InstructionDivisionFloat64{}
+	i.Env = concurrentmap.NewConcurrentMap()
+	i.Env.Set("var1", &interpreter.Variable{Value: "11.11"})
+	i.Env.Set("var2", &interpreter.Variable{Value: "22.22"})
+	i.Env.Set("var3", &interpreter.Variable{Value: "0"})
+
+	i.Execute("var1", "var2")
+	if v, _ := i.Env.Get("var1"); v.(*interpreter.Variable).Value != "0.5" {
+		t.Errorf(
+			"\nEXPECT: %v\nGET: %v\n",
+			"0.50",
+			v.(*interpreter.Variable).Value,
+		)
+	}
+
+	i.Execute("var1", "33.33")
+	if v, _ := i.Env.Get("var1"); v.(*interpreter.Variable).Value != "0.01500150015" {
+		t.Errorf(
+			"\nEXPECT: %v\nGET: %v\n",
+			"0.01500150015",
+			v.(*interpreter.Variable).Value,
+		)
+	}
+
+	_, err := i.Execute("var1", "0")
+	if !strings.Contains(err.Error(), "quotition") {
+		t.Errorf(
+			"\nEXPECT: %v\nGET: %v\n",
+			"inf quotition",
+			err.Error(),
+		)
+	}
+
+	i.Execute("var3", "33.33")
+	if v, _ := i.Env.Get("var3"); v.(*interpreter.Variable).Value != "0" {
+		t.Errorf(
+			"\nEXPECT: %v\nGET: %v\n",
+			"0",
+			v.(*interpreter.Variable).Value,
+		)
+	}
+
+}
