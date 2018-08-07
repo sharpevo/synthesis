2f0b682f60c27cc85859a6815521f7b7afff237c
yangwu <yang.wu@igenetech.com>
2018-08-07 13:56:49 +0800

    Add InstructionMultiplicationFloat64 and test case #318

---
+++ arithmetic.mul.float64_test.go
@@ -0,0 +1,33 @@
+package instruction_test
+
+import (
+	"posam/instruction"
+	"posam/interpreter"
+	"posam/util/concurrentmap"
+	"testing"
+)
+
+func TestInstructionAdditionFloat64Execute(t *testing.T) {
+	i := instruction.InstructionMultiplicationFloat64{}
+	i.Env = concurrentmap.NewConcurrentMap()
+	i.Env.Set("var1", &interpreter.Variable{Value: "11.11"})
+	i.Env.Set("var2", &interpreter.Variable{Value: "22.22"})
+
+	i.Execute("var1", "var2")
+	if v, _ := i.Env.Get("var1"); v.(*interpreter.Variable).Value != "246.8642" {
+		t.Errorf(
+			"\nEXPECT: %v\nGET: %v\n",
+			"246.8642",
+			v.(*interpreter.Variable).Value,
+		)
+	}
+
+	i.Execute("var1", "33.33")
+	if v, _ := i.Env.Get("var1"); v.(*interpreter.Variable).Value != "8227.983786" {
+		t.Errorf(
+			"\nEXPECT: %v\nGET: %v\n",
+			"8227.983786",
+			v.(*interpreter.Variable).Value,
+		)
+	}
+}
