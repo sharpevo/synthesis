b0fab104a0b7e5d839948678c9161c54faa84a60
yangwu <yang.wu@igenetech.com>
2018-08-11 10:20:00 +0800

    Add InstructionPrinterHeadErrorCode #314

---
+++ printerhead.errorcode.go
@@ -0,0 +1,24 @@
+package instruction
+
+import (
+	"fmt"
+	"posam/dao/ricoh_g5"
+)
+
+type InstructionPrinterHeadErrorCode struct {
+	Instruction
+}
+
+func (i *InstructionPrinterHeadErrorCode) Execute(args ...string) (resp interface{}, err error) {
+	if len(args) != 1 {
+		return resp, fmt.Errorf("not enough arguments")
+	}
+
+	variable, _ := i.ParseVariable(args[0])
+	resp, err = ricoh_g5.Instance("").QueryErrorCode()
+	variable.Value = resp
+	if err != nil {
+		return variable.Value, err
+	}
+	return
+}
