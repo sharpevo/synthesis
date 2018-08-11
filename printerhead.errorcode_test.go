3dedbd1893e4d47dee20b4a293897472c28689f6
yangwu <yang.wu@igenetech.com>
2018-08-11 14:58:38 +0800

    Add test case for real server #314

--- printerhead.errorcode_test.go
+++ printerhead.errorcode_test.go
@@ -15,7 +15,7 @@ import (
 )
 
 var ServerNetwork = "tcp"
-var ServerAddress = "localhost:6507"
+var ServerAddress = "localhost:21005"
 
 func TestMain(m *testing.M) {
 	ricoh_g5.AddInstance(&ricoh_g5.Dao{
@@ -130,5 +130,63 @@ func TestInstructionPrinterHeadErrorCodeExecute(t *testing.T) {
 		conn.Close()
 	}
 	<-completec
+
+}
+
+func xTestInstructionPrinterHeadErrorCodeExecuteForRealServer(t *testing.T) {
+	testList := []struct {
+		args            []string
+		response        []byte
+		expectedRequest []byte
+		errString       string
+	}{
+		{
+			args: []string{"var1"},
+			response: []byte{
+				0x01, 0x00, 0x00, 0x00,
+				0x00, 0x00, 0x00, 0x00,
+			},
+		},
+		{
+			args: []string{"var1"},
+			response: []byte{
+				0x02, 0x00, 0x00, 0x00,
+				0x00, 0x00, 0x00, 0x00,
+			},
+			errString: "response error",
+		},
+	}
+	i := instruction.InstructionPrinterHeadErrorCode{}
+	i.Env = concurrentmap.NewConcurrentMap()
+	for k, test := range testList {
+		resp, err := i.Execute(test.args...)
+		t.Logf("#%d, %v, %v", k, resp, err)
+		if err != nil {
+			if test.errString != "" && strings.Contains(err.Error(), test.errString) {
+				t.Logf("error occured as expected %s", err)
+
+				// no things to be sent if error occurred
+				// send a message to server to unblock l.Accept()
+				//ricoh_g5.Instance(ServerAddress).QueryErrorCode()
+
+				fmt.Println("err")
+				continue
+			} else {
+				// panic if change errString to "foo"
+				panic(err)
+			}
+		}
+		v, _ := i.Env.Get(test.args[0])
+		actual := v.(*interpreter.Variable).Value
+		// save to the stack
+		if !bytes.Equal(actual.([]byte), resp.([]byte)) {
+			t.Errorf(
+				"\nEXPECT: '%s'\nGET:    '%x'\n",
+				resp,
+				actual,
+			)
+		}
+	}
+	//ricoh_g5.Instance("").TCPClient.Instance().Close()
 
 }
