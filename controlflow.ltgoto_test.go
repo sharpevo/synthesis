b93a236cbb75ca2db4f81b256f552f7344e63421
yangwu <yang.wu@igenetech.com>
2018-08-22 11:01:22 +0800

    Add InstructionControlFlowLessThanGoto #320

---
+++ controlflow.ltgoto_test.go
@@ -0,0 +1,34 @@
+package instruction_test
+
+import (
+	"posam/instruction"
+	"posam/interpreter"
+	"posam/interpreter/vrb"
+	"testing"
+)
+
+func TestInstructionControlFlowLessThanGotoExecute(t *testing.T) {
+	i := instruction.InstructionControlFlowLessThanGoto{}
+	i.Env = interpreter.NewStack()
+	i.Execute("5")
+	varCur, _ := i.Env.Get("SYS_CUR")
+	actual := varCur.Value.(int64)
+	if actual != 0 {
+		t.Errorf(
+			"\nEXPECT: %v\nGET: %v\n",
+			0,
+			actual,
+		)
+	}
+	varErr, _ := i.Env.Get("SYS_CMP")
+	varErr.Value = vrb.LESS
+	i.Execute("10")
+	actual = varCur.Value.(int64)
+	if actual != 10 {
+		t.Errorf(
+			"\nEXPECT: %v\nGET: %v\n",
+			10,
+			actual,
+		)
+	}
+}
