e0b6a54b71514b5edf7bab0b36a9e25f52978dae
yangwu <yang.wu@igenetech.com>
2018-08-22 09:58:32 +0800

    Add argument length checking for arithmetic instructions

--- arithmetic.div.float64.go
+++ arithmetic.div.float64.go
@@ -9,6 +9,9 @@ type InstructionDivisionFloat64 struct {
 }
 
 func (i *InstructionDivisionFloat64) Execute(args ...string) (resp interface{}, err error) {
+	if len(args < 2) {
+		return resp, fmt.Errorf("not enough arguments")
+	}
 	variable, v1, v2, err := i.ParseObjects(args[0], args[1])
 	if err != nil {
 		return resp, err
--- arithmetic.mul.float64.go
+++ arithmetic.mul.float64.go
@@ -7,6 +7,9 @@ type InstructionMultiplicationFloat64 struct {
 }
 
 func (i *InstructionMultiplicationFloat64) Execute(args ...string) (resp interface{}, err error) {
+	if len(args < 2) {
+		return resp, fmt.Errorf("not enough arguments")
+	}
 	variable, v1, v2, err := i.ParseObjects(args[0], args[1])
 	if err != nil {
 		return resp, err
