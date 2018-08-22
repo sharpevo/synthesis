ae4138573a3275043793f63659d9469627c39c5a
yangwu <yang.wu@igenetech.com>
2018-08-22 08:13:12 +0800

    Add InstructionControlFlowErrGoto #320

---
+++ controlflow.errgoto.go
@@ -0,0 +1,28 @@
+package instruction
+
+import (
+	"fmt"
+	"strconv"
+)
+
+type InstructionControlFlowErrGoto struct {
+	InstructionControlFlowGoto
+}
+
+func (i *InstructionControlFlowErrGoto) Execute(args ...string) (resp interface{}, err error) {
+	if len(args) < 1 {
+		return resp, fmt.Errorf("not enough arguments")
+	}
+	index, err := strconv.ParseInt(args[0], 0, 64)
+	if err != nil {
+		return resp, fmt.Errorf("invalid argument %q: %s", args[0], err.Error())
+	}
+	v, found := i.Env.Get("SYS_ERR")
+	if !found {
+		return resp, fmt.Errorf("failed to load variable ERR")
+	}
+	if v.Value.(string) != "" {
+		i.Goto(index)
+	}
+	return resp, nil
+}
---
+++ controlflow.errgoto_test.go
@@ -0,0 +1,33 @@
+package instruction_test
+
+import (
+	"posam/instruction"
+	"posam/interpreter"
+	"testing"
+)
+
+func TestInstructionControlFlowErrGotoExecute(t *testing.T) {
+	i := instruction.InstructionControlFlowErrGoto{}
+	i.Env = interpreter.NewStack()
+	i.Execute("5")
+	varCur, _ := i.Env.Get("SYS_CUR")
+	actual := varCur.Value.(int64)
+	if actual != 0 {
+		t.Errorf(
+			"\nEXPECT: %v\nGET: %v\n",
+			0,
+			actual,
+		)
+	}
+	varErr, _ := i.Env.Get("SYS_ERR")
+	varErr.Value = "err occured"
+	i.Execute("10")
+	actual = varCur.Value.(int64)
+	if actual != 10 {
+		t.Errorf(
+			"\nEXPECT: %v\nGET: %v\n",
+			10,
+			actual,
+		)
+	}
+}
