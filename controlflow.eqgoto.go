f23ded757fa8b70751e9abcbbc18a05c2b978454
yangwu <yang.wu@igenetech.com>
2018-08-22 10:14:06 +0800

    Add InstructionControlFlowEqualGoto #320

---
+++ controlflow.eqgoto.go
@@ -0,0 +1,31 @@
+package instruction
+
+import (
+	"fmt"
+	"posam/interpreter/vrb"
+	"strconv"
+)
+
+type InstructionControlFlowEqualGoto struct {
+	InstructionControlFlowGoto
+}
+
+func (i *InstructionControlFlowEqualGoto) Execute(args ...string) (resp interface{}, err error) {
+	if len(args) < 1 {
+		return resp, fmt.Errorf("not enough arguments")
+	}
+	index, err := strconv.ParseInt(args[0], 0, 64)
+	if err != nil {
+		return resp, fmt.Errorf("invalid argument %q: %s", args[0], err.Error())
+	}
+	v, found := i.Env.Get("SYS_CMP")
+	if !found {
+		return resp, fmt.Errorf("failed to load variable CMP")
+	}
+	resp = fmt.Sprintf("condition cheking passed and continue")
+	if v.Value == vrb.EQUAL {
+		i.Goto(index)
+		resp = fmt.Sprintf("condition satisfied and goto %d", index)
+	}
+	return resp, nil
+}
