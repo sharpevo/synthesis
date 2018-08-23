8120af4b4890398dfc48cc7ffac8325f13ff96d0
yangwu <yang.wu@igenetech.com>
2018-08-23 09:29:56 +0800

    Add InstructionTree #309

---
+++ tree.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"fmt"
+	"github.com/therecipe/qt/core"
+	"github.com/therecipe/qt/widgets"
+)
+
+type InstructionTree struct {
+	widgets.QTreeWidget
+	contextMenu *widgets.QMenu
+}
+
+func NewInstructionTree() *InstructionTree {
+
+	treeWidget := &InstructionTree{*widgets.NewQTreeWidget(nil), nil}
+	treeWidget.SetWindowTitle("Graphical Programming")
+	treeWidget.SetContextMenuPolicy(core.Qt__CustomContextMenu)
+	treeWidget.ConnectCustomContextMenuRequested(treeWidget.customContextMenuRequested)
+	rootNode := treeWidget.InvisibleRootItem()
+	anItem := widgets.NewQTreeWidgetItem2([]string{"first instruction"}, 0)
+	rootNode.AddChild(anItem)
+	treeWidget.ExpandAll()
+
+	return treeWidget
+}
+
+func (w *InstructionTree) customContextMenuRequested(p *core.QPoint) {
+	if w.contextMenu == nil {
+		w.contextMenu = widgets.NewQMenu(w)
+		menuAdd := w.contextMenu.AddAction("Add")
+		menuAdd.ConnectTriggered(func(checked bool) { w.addItem(p) })
+		menuRemove := w.contextMenu.AddAction("Remove")
+		menuRemove.ConnectTriggered(func(checked bool) { w.removeItem(p) })
+	}
+	w.contextMenu.Exec2(w.MapToGlobal(p), nil)
+}
+
+func (t *InstructionTree) addItem(p *core.QPoint) {
+	root := t.ItemAt(p)
+	if root.Pointer() == nil {
+		root = t.InvisibleRootItem()
+	}
+	item := widgets.NewQTreeWidgetItem2([]string{"instruction"}, 0)
+	//item.SetFlags(^core.Qt__ItemIsEditable)
+	root.AddChild(item)
+	root.SetExpanded(true)
+}
+
+func (t *InstructionTree) removeItem(p *core.QPoint) {
+	item := t.ItemAt(p)
+	fmt.Println(item.Text(0))
+	item.Parent().RemoveChild(item)
+}
