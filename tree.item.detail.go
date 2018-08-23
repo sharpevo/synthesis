768b24d240d79173467195956f9540af195cb337
yangwu <yang.wu@igenetech.com>
2018-08-23 15:50:50 +0800

    Add instruction type combobox #309

--- tree.item.detail.go
+++ tree.item.detail.go
@@ -2,6 +2,12 @@ package main
 
 import (
 	"github.com/therecipe/qt/widgets"
+	"strings"
+)
+
+const (
+	TYPE_INS = "Instruction"
+	TYPE_SET = "Instruction Set"
 )
 
 type InstructionDetail struct {
@@ -9,22 +15,35 @@ type InstructionDetail struct {
 	GroupBox   *widgets.QGroupBox
 	titleInput *widgets.QLineEdit
 	lineInput  *widgets.QLineEdit
+	typeInput  *widgets.QComboBox
 	saveButton *widgets.QPushButton
 }
 
 func NewInstructionDetail() *InstructionDetail {
+
+	typeLabel := widgets.NewQLabel2("Type", nil, 0)
+	titleLabel := widgets.NewQLabel2("Title", nil, 0)
+	lineLabel := widgets.NewQLabel2("Instruction", nil, 0)
+
 	d := InstructionDetail{}
+	d.typeInput = widgets.NewQComboBox(nil)
+	d.typeInput.AddItems([]string{TYPE_INS, TYPE_SET})
 	d.titleInput = widgets.NewQLineEdit(nil)
 	d.lineInput = widgets.NewQLineEdit(nil)
+
 	d.saveButton = widgets.NewQPushButton2("SAVE", nil)
 	d.saveButton.ConnectClicked(func(bool) { d.saveInstruction() })
 	d.saveButton.SetEnabled(false)
 
 	d.GroupBox = widgets.NewQGroupBox2("Instruction", nil)
 	layout := widgets.NewQGridLayout2()
-	layout.AddWidget(d.titleInput, 0, 0, 0)
-	layout.AddWidget(d.lineInput, 1, 0, 0)
-	layout.AddWidget(d.saveButton, 2, 0, 0)
+	layout.AddWidget(typeLabel, 0, 0, 0)
+	layout.AddWidget(d.typeInput, 0, 1, 0)
+	layout.AddWidget(titleLabel, 1, 0, 0)
+	layout.AddWidget(d.titleInput, 1, 1, 0)
+	layout.AddWidget(lineLabel, 2, 0, 0)
+	layout.AddWidget(d.lineInput, 2, 1, 0)
+	layout.AddWidget3(d.saveButton, 3, 0, 1, 2, 0)
 	d.GroupBox.SetLayout(layout)
 	return &d
 }
@@ -42,5 +61,20 @@ func (d *InstructionDetail) Refresh(item *widgets.QTreeWidgetItem) {
 	d.treeItem = item
 	d.titleInput.SetText(item.Text(0))
 	d.lineInput.SetText(line)
+	d.SetTypeInput()
 	d.saveButton.SetEnabled(true)
+}
+
+func (d *InstructionDetail) TypeInput() string {
+	return d.typeInput.CurrentText()
+}
+
+func (d *InstructionDetail) SetTypeInput() {
+	if strings.HasPrefix(d.lineInput.Text(), "ASYNC") ||
+		strings.HasPrefix(d.lineInput.Text(), "IMPORT") {
+		d.typeInput.SetCurrentText(TYPE_SET)
+	} else {
+		d.typeInput.SetCurrentText(TYPE_INS)
+	}
+
 }
