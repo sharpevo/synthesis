01cad789f81df957bf0e6ffc8d7de717e65501cf
yangwu <yang.wu@igenetech.com>
2018-08-28 10:04:17 +0800

    Add utils of ui

---
+++ uiutil/util.go
@@ -0,0 +1,35 @@
+package uiutil
+
+import (
+	"fmt"
+	"github.com/therecipe/qt/widgets"
+)
+
+func FilePath() (string, error) {
+	dialog := widgets.NewQFileDialog2(nil, "Select file...", "", "")
+	if dialog.Exec() != int(widgets.QDialog__Accepted) {
+		return "", fmt.Errorf("nothing selected")
+	}
+	filePath := dialog.SelectedFiles()[0]
+	return filePath, nil
+}
+
+func MessageBoxInfo(message string) {
+	widgets.QMessageBox_Information(
+		nil,
+		"Information",
+		message,
+		widgets.QMessageBox__Ok,
+		widgets.QMessageBox__Close,
+	)
+}
+
+func MessageBoxError(message string) {
+	widgets.QMessageBox_Critical(
+		nil,
+		"Error",
+		message,
+		widgets.QMessageBox__Ok,
+		widgets.QMessageBox__Close,
+	)
+}
