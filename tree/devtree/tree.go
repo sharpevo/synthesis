04d2d5084f8b1c8fa5ffdd6267cc723fff5ac87b
yangwu <yang.wu@igenetech.com>
2018-08-28 10:49:07 +0800

    Export and import device configuration automatically #333

--- tree/devtree/tree.go
+++ tree/devtree/tree.go
@@ -1,10 +1,15 @@
 package devtree
 
 import (
+	"fmt"
 	"github.com/therecipe/qt/core"
 	"github.com/therecipe/qt/widgets"
 	"posam/gui/tree"
 	"posam/gui/uiutil"
+)
+
+const (
+	DEVICE_CONF_FILEPATH = "devices.bin"
 )
 
 type DeviceTree struct {
@@ -27,9 +32,12 @@ func NewTree(
 	treeWidget.SetContextMenuPolicy(core.Qt__CustomContextMenu)
 	treeWidget.ConnectCustomContextMenuRequested(treeWidget.customContextMenuRequested)
 	treeWidget.ConnectItemClicked(treeWidget.customItemClicked)
-	rootNode := treeWidget.InvisibleRootItem()
-	anItem := NewDeviceItem("Root Device", "value")
-	rootNode.AddChild(anItem)
+	treeWidget.ConnectItemChanged(treeWidget.customItemChanged)
+
+	err := treeWidget.Import()
+	if err != nil {
+		uiutil.MessageBoxError(err.Error())
+	}
 
 	treeWidget.SetAcceptDrops(true)
 	treeWidget.SetDragEnabled(true)
@@ -57,6 +65,14 @@ func (t *DeviceTree) customItemClicked(item *widgets.QTreeWidgetItem, column int
 	t.detail.Refresh(item)
 }
 
+func (t *DeviceTree) customItemChanged(item *widgets.QTreeWidgetItem, column int) {
+	err := t.Save()
+	if err != nil {
+		uiutil.MessageBoxError(err.Error())
+		return
+	}
+}
+
 func (t *DeviceTree) customContextMenuRequested(p *core.QPoint) {
 	if t.ContextMenu == nil {
 		t.ContextMenu = widgets.NewQMenu(t)
@@ -71,12 +87,43 @@ func (t *DeviceTree) customContextMenuRequested(p *core.QPoint) {
 func (t *DeviceTree) Save() error {
 	item := t.InvisibleRootItem()
 	node := t.ExportNode(item)
-	err := node.Write("/tmp/exportnode.gob")
+	err := tree.ExportNode(node, DEVICE_CONF_FILEPATH)
 	if err != nil {
 		return err
 	}
-	uiutil.MessageBoxInfo("Exported")
+	uiutil.MessageBoxInfo(fmt.Sprintf(
+		"Configuration is saved as %q", DEVICE_CONF_FILEPATH))
 	return nil
+}
+
+func (t *DeviceTree) Import() error {
+	node := new(Node)
+	err := tree.ImportNode(node, DEVICE_CONF_FILEPATH)
+	if err != nil {
+		if err.Error() == "nothing selected" {
+			return nil
+		}
+		return err
+	}
+	t.Clear()
+	for i := 0; i < len(node.Children); i++ {
+		t.InvisibleRootItem().AddChild(t.ImportNode(node.Children[i]))
+	}
+	t.ExpandAll()
+	return nil
+}
+
+func (t *DeviceTree) ImportNode(node Node) *widgets.QTreeWidgetItem {
+	item := widgets.NewQTreeWidgetItem2([]string{node.Title}, 0)
+	item.SetData(
+		0,
+		tree.DataRole(),
+		core.NewQVariant17(node.Data),
+	)
+	for i := 0; i < len(node.Children); i++ {
+		item.AddChild(t.ImportNode(node.Children[i]))
+	}
+	return item
 }
 
 func (t *DeviceTree) ExportNode(root *widgets.QTreeWidgetItem) Node {
