3823f94d19e29c8c5776681b1a677175f459deae
yangwu <yang.wu@igenetech.com>
2018-08-30 09:45:49 +0800

    Accept float and int for InstructionSubtraction #318

--- arithmetic.sub_test.go
+++ arithmetic.sub_test.go
@@ -8,8 +8,8 @@ import (
 	"testing"
 )
 
-func TestInstructionSubtractionFloat64Execute(t *testing.T) {
-	i := instruction.InstructionSubtractionFloat64{}
+func TestInstructionSubtractionExecuteFloat64(t *testing.T) {
+	i := instruction.InstructionSubtraction{}
 	i.Env = interpreter.NewStack()
 	var1, _ := vrb.NewVariable("var1", "88.88")
 	var2, _ := vrb.NewVariable("var2", "11.11")
@@ -30,6 +30,33 @@ func TestInstructionSubtractionFloat64Execute(t *testing.T) {
 		t.Errorf(
 			"\nEXPECT: %v\nGET: %v\n",
 			"55.55",
+			v.Value,
+		)
+	}
+}
+
+func TestInstructionSubtractionExecuteInt64(t *testing.T) {
+	i := instruction.InstructionSubtraction{}
+	i.Env = interpreter.NewStack()
+	var1, _ := vrb.NewVariable("var1", "11")
+	var2, _ := vrb.NewVariable("var2", "22")
+	i.Env.Set(var1)
+	i.Env.Set(var2)
+
+	i.Execute(var1.Name, var2.Name)
+	if v, _ := i.Env.Get(var1.Name); v.Value.(int64) != -11 {
+		t.Errorf(
+			"\nEXPECT: %v\nGET: %v\n",
+			-11,
+			v.Value,
+		)
+	}
+
+	i.Execute(var1.Name, "33")
+	if v, _ := i.Env.Get(var1.Name); v.Value.(int64) != -44 {
+		t.Errorf(
+			"\nEXPECT: %v\nGET: %v\n",
+			-44,
 			v.Value,
 		)
 	}
