a355af71c2cf68802b9a3539b7f0f38b865ecbba
yangwu <yang.wu@igenetech.com>
2018-09-21 13:45:07 +0800

    Add variable support

--- tml.move.go
+++ tml.move.go
@@ -32,15 +32,28 @@ func (i *InstructionTMLMove) Initialize(args ...string) (
 		return instance, pos, speed, accel,
 			fmt.Errorf("device %q is not initialized", args[0])
 	}
-	pos, err = strconv.ParseFloat(args[1], 64)
+	pos, err = i.ParseFloat(args[1])
 	if err != nil {
 		return instance, pos, speed, accel, err
 	}
-	speed, err = strconv.ParseFloat(args[2], 64)
+	speed, err = i.ParseFloat(args[2])
 	if err != nil {
 		return instance, pos, speed, accel, err
 	}
-	accel, err = strconv.ParseFloat(args[3], 64)
+	accel, err = i.ParseFloat(args[3])
+	if err != nil {
+		return instance, pos, speed, accel, err
+	}
+
+	pos, err = i.ParseFloat(args[1])
+	if err != nil {
+		return instance, pos, speed, accel, err
+	}
+	speed, err = i.ParseFloat(args[2])
+	if err != nil {
+		return instance, pos, speed, accel, err
+	}
+	accel, err = i.ParseFloat(args[3])
 	if err != nil {
 		return instance, pos, speed, accel, err
 	}
@@ -70,31 +83,42 @@ func (i *InstructionTMLMove) InitializeDual(args ...string) (
 		return instance, posx, posy, speed, accel,
 			fmt.Errorf("device %q is not initialized", args[0])
 	}
-	posxVar, found := i.Env.Get(args[1])
-	if !found {
-		return instance, posx, posy, speed, accel,
-			fmt.Errorf("invalid variable %q", args[1])
-	}
-	posx, err = strconv.ParseFloat(fmt.Sprintf("%v", posxVar.Value), 64)
+	posx, err = i.ParseFloat(args[1])
 	if err != nil {
 		return instance, posx, posy, speed, accel, err
 	}
-	posyVar, found := i.Env.Get(args[2])
-	if !found {
-		return instance, posx, posy, speed, accel,
-			fmt.Errorf("invalid variable %q", args[2])
-	}
-	posy, err = strconv.ParseFloat(fmt.Sprintf("%v", posyVar.Value), 64)
+	posy, err = i.ParseFloat(args[2])
 	if err != nil {
 		return instance, posx, posy, speed, accel, err
 	}
-	speed, err = strconv.ParseFloat(args[3], 64)
+	speed, err = i.ParseFloat(args[3])
 	if err != nil {
 		return instance, posx, posy, speed, accel, err
 	}
-	accel, err = strconv.ParseFloat(args[4], 64)
+	accel, err = i.ParseFloat(args[4])
 	if err != nil {
 		return instance, posx, posy, speed, accel, err
 	}
 	return
+}
+
+func (i *InstructionTMLMove) ParseFloat(input string) (output float64, err error) {
+	outputVar, found := i.Env.Get(input)
+	if !found {
+		output, err = strconv.ParseFloat(input, 64)
+		if err != nil {
+			return output, err
+		}
+	} else {
+		output, err = strconv.ParseFloat(fmt.Sprintf("%v", outputVar.Value), 64)
+		if err != nil {
+			return output,
+				fmt.Errorf(
+					"failed to parse variable %q to float: %s",
+					input,
+					err.Error(),
+				)
+		}
+	}
+	return output, nil
 }
