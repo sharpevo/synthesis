b8fc07543182e978664466e4725cfb453676129f
yangwu <yang.wu@igenetech.com>
2018-10-11 13:25:56 +0800

    Add InstructionCANSensorOxygenConc for testing #360

---
+++ can.sensor.oxygen.conc.go
@@ -0,0 +1,32 @@
+package instruction
+
+import (
+	"fmt"
+	"posam/dao/canalystii"
+)
+
+func init() {
+	canalystii.InstructionMap.Set("OXYGENCONC", InstructionCANSensorOxygenConc{})
+}
+
+type InstructionCANSensorOxygenConc struct {
+	InstructionCAN
+}
+
+func (i *InstructionCANSensorOxygenConc) Execute(args ...string) (resp interface{}, err error) {
+	if len(args) < 2 {
+		return resp, fmt.Errorf("not enough arguments")
+	}
+	instance, err := i.ParseDevice(args[0])
+	if err != nil {
+		return resp, err
+	}
+	variable, err := i.ParseVariable(args[1])
+	if err != nil {
+		return resp, err
+	}
+	resp, err = instance.ReadOxygenConc()
+	// TODO: calculate concentration
+	variable.Value = resp
+	return resp, err
+}
