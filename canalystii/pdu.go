9615a0fae3e0e9e5615b0845bf1ec99c68d1028a
yangwu <yang.wu@igenetech.com>
2018-10-11 17:08:03 +0800

    Add PDUs #360

--- canalystii/pdu.go
+++ canalystii/pdu.go
@@ -2,12 +2,68 @@ package canalystii
 
 import (
 	"posam/dao"
+	"posam/protocol/usbcan"
 )
 
-var SensorOxygenConcUnit,
-	SensorHumitureUnit dao.Unit
+var MotorMoveRelativeUnit,
+	MotorMoveAbsoluteUnit,
+	MotorResetUnit,
+	SwitcherControlUnit,
+	SwitcherControlAdvancedUnit,
+	SensorHumitureUnit,
+	SensorOxygenConcUnit,
+	SystemRomReadUnit,
+	SystemRomWriteUnit dao.Unit
 
 func init() {
+	// MotorMoveRelativeUnit{{{
+	MotorMoveRelativeRequest := dao.Request{}
+	MotorMoveRelativeRequest.Function = 0x01
+	MotorMoveRelativeUnit.SetRequest(MotorMoveRelativeRequest)
+	MotorMoveRelativeUnit.SetRecResp(responseReceived())
+	MotorMoveRelativeUnit.SetComResp(responseCompleted())
+	// }}}
+
+	// MotorResetUnit{{{
+	MotorResetRequest := dao.Request{}
+	MotorResetRequest.Function = 0x02
+	MotorResetUnit.SetRequest(MotorResetRequest)
+	MotorResetUnit.SetRecResp(responseReceived())
+	MotorResetUnit.SetComResp(responseCompleted())
+	// }}}
+
+	// MotorMoveAbsoluteUnit{{{
+	MotorMoveAbsoluteRequest := dao.Request{}
+	MotorMoveAbsoluteRequest.Function = 0x03
+	MotorMoveAbsoluteUnit.SetRequest(MotorMoveAbsoluteRequest)
+	MotorMoveAbsoluteUnit.SetRecResp(responseReceived())
+	MotorMoveAbsoluteUnit.SetComResp(responseCompleted())
+	// }}}
+
+	// SwitcherControlUnit{{{
+	SwitcherControlRequest := dao.Request{}
+	SwitcherControlRequest.Function = 0x0A
+	SwitcherControlUnit.SetRequest(SwitcherControlRequest)
+	SwitcherControlUnit.SetRecResp(responseNil())
+	SwitcherControlUnit.SetComResp(responseNil())
+	// }}}
+
+	// SwitcherControlAdvancedUnit{{{
+	SwitcherControlAdvancedRequest := dao.Request{}
+	SwitcherControlAdvancedRequest.Function = 0x0B
+	SwitcherControlAdvancedUnit.SetRequest(SwitcherControlAdvancedRequest)
+	SwitcherControlAdvancedUnit.SetRecResp(responseNil())
+	SwitcherControlAdvancedUnit.SetComResp(responseNil())
+	// }}}
+
+	// SensorHumitureUnit{{{
+	SensorHumitureRequest := dao.Request{}
+	SensorHumitureRequest.Function = 0x0C
+	SensorHumitureUnit.SetRequest(SensorHumitureRequest)
+	SensorHumitureUnit.SetRecResp(responseNil())
+	SensorHumitureUnit.SetComResp(responseNil())
+	// }}}
+
 	// SensorOxygenConcUnit{{{
 	SensorOxygenConcRequest := dao.Request{}
 	SensorOxygenConcRequest.Function = 0x0D
@@ -20,7 +76,35 @@ func init() {
 		0x00,
 	}
 	SensorOxygenConcUnit.SetRequest(SensorOxygenConcRequest)
-	SensorOxygenConcUnit.SetRecResp([]byte{})
-	SensorOxygenConcUnit.SetComResp([]byte{})
+	SensorOxygenConcUnit.SetRecResp(responseNil())
+	SensorOxygenConcUnit.SetComResp(responseNil())
 	// }}}
+
+	// SystemRomReadUnit{{{
+	SystemRomReadRequest := dao.Request{}
+	SystemRomReadRequest.Function = 0xF0
+	SystemRomReadUnit.SetRequest(SystemRomReadRequest)
+	SystemRomReadUnit.SetRecResp(responseNil())
+	SystemRomReadUnit.SetComResp(responseNil())
+	// }}}
+
+	// SystemRomWriteUnit{{{
+	SystemRomWriteRequest := dao.Request{}
+	SystemRomWriteRequest.Function = 0xF1
+	SystemRomWriteUnit.SetRequest(SystemRomReadRequest)
+	SystemRomWriteUnit.SetRecResp(responseNil())
+	SystemRomWriteUnit.SetComResp(responseNil())
+	// }}}
+}
+
+func responseReceived() []byte {
+	return []byte{usbcan.STATUS_CODE_RECEIVED}
+}
+
+func responseCompleted() []byte {
+	return []byte{usbcan.STATUS_CODE_COMPLETED}
+}
+
+func responseNil() []byte {
+	return []byte{}
 }
