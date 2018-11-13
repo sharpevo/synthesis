08f336872dd68bf75cdff3cb1338711e92e03828
yangwu <yang.wu@igenetech.com>
2018-11-13 11:26:57 +0800

    Add TestPrint

--- printheads/dao_test.go
+++ printheads/dao_test.go
@@ -2,9 +2,15 @@ package printheads_test
 
 import (
 	"fmt"
+	"image"
+	"image/png"
+	"math"
+	"os"
 	"posam/dao/printheads"
+	"posam/util/platform"
 	"reflect"
 	"testing"
+	"time"
 )
 
 const (
@@ -305,4 +311,160 @@ func TestUpdatePosition(t *testing.T) {
 			actual,
 		)
 	}
+}
+
+func TestPrint(t *testing.T) {
+
+	fmt.Println("====")
+	fmt.Println()
+	fmt.Println()
+	p := platform.NewPlatform(100, 100)
+	myImage := image.NewRGBA(image.Rect(0, 0, p.Width, p.Height))
+	block1 := &platform.Block{}
+	block1.PositionX = 25
+	block1.PositionY = 25
+	block1.SpaceX = 2
+	block1.SpaceY = 5
+	block1.AddRow("TTTTTCTGGA")
+	block1.AddRow("AGGTGCGTGT")
+	block1.AddRow("GGAGGGAATG")
+	block1.AddRow("CTGTGCGTGA")
+	minWidth := 10 + block1.SpaceX*(10-1) + block1.PositionX
+	minHeight := 4 + block1.SpaceY*(4-1) + block1.PositionY
+	fmt.Println("min platform: ", minWidth, minHeight)
+	p.AddBlock(block1)
+	for posy, row := range p.Dots {
+		for posx, dot := range row {
+			if dot == nil {
+				continue
+			}
+			myImage.Set(posx, posy, dot.Base.Color)
+		}
+	}
+	outputFile, _ := os.Create("test.png")
+	png.Encode(outputFile, myImage)
+	outputFile.Close()
+
+	// printhead
+	//h, _ := printheads.NewPrintHead(
+	//ROW_COUNT,
+	//NOZZLE_COUNT,
+	//NOZZLE_SPACE,
+	//ROW_OFFSET,
+	//ROW_SPACE_A,
+	//ROW_SPACE_B,
+	//)
+	h, _ := printheads.NewPrintHeadLineD(
+		ROW_COUNT,
+		NOZZLE_COUNT,
+		NOZZLE_SPACE,
+		ROW_OFFSET,
+		ROW_SPACE_A,
+		ROW_SPACE_B,
+		0,
+		0,
+	)
+	for _, v := range NozzleMap1 {
+		fmt.Printf("%s\n", v.String())
+	}
+	time.Sleep(time.Second)
+
+	// read
+	pf, _ := platform.ParsePlatform("test.png")
+
+	// next dots
+	px, py, err := pf.NextPosition()
+	if err != nil {
+		fmt.Println(err)
+	}
+	hx, hy := ToPrintheadPosition(px, py)
+	h.UpdatePositionStar(hx*printheads.MM, hy*printheads.MM)
+
+	// loop horizontally, from left to right
+	// 1. offset, upward
+	// 2. next, downward
+	imageIndex := 0
+	img := image.NewRGBA(image.Rect(0, 0, pf.Width, pf.Height))
+	for h.Rows[3].Nozzles[0].X < 50*printheads.MM {
+
+		// loop vertically, from printhead bottom to printhead top
+		// downward
+		fmt.Println(">>>downward")
+		for dposy := hy * printheads.MM; h.Rows[3].Nozzles[0].Y > -50*printheads.MM; dposy -= h.RowOffset {
+			genData(h, pf, py, &imageIndex, img)
+			h.UpdatePositionStar(hx*printheads.MM, dposy)
+		}
+
+		dposx := hx*printheads.MM + h.RowOffset
+		h.UpdatePositionStar(dposx, h.Rows[0].Nozzles[0].Y)
+
+		// upward
+		fmt.Println(">>>upword")
+		for dposy := h.Rows[0].Nozzles[0].Y; h.Rows[0].Nozzles[0].Y < 50*printheads.MM; dposy += h.RowOffset {
+			genData(h, pf, py, &imageIndex, img)
+			h.UpdatePositionStar(dposx, dposy)
+		}
+
+		px, py, err = pf.NextPosition()
+		if err != nil {
+			break
+		}
+		hx, hy = ToPrintheadPosition(px, py)
+		//fmt.Println(hx, hy, h.Rows[3].Nozzles[0].X)
+		h.UpdatePositionStar(hx*printheads.MM, hy*printheads.MM)
+	}
+}
+
+func printheadPosition(h *printheads.PrintHead) {
+	fmt.Println("printhead left bottom: ", h.Rows[0].X, h.Rows[0].Y)
+}
+
+func moveAbs(x int, y int) {
+	hx, hy := ToPrintheadPosition(x, y)
+	fmt.Printf("abs move dot (%v, %v), printhead (%v, %v)\n", x, y, hx, hy)
+}
+
+func ToPrintheadPosition(x int, y int) (int, int) {
+	return x - 50, 50 - y
+}
+func ToPlatformPosition(x int, y int) (int, int) {
+	return x + 50, 50 - y
+}
+
+func genData(h *printheads.PrintHead, pf *platform.Platform, py int, imageIndex *int, img *image.RGBA) []int {
+	data := make([]int, 1280)
+
+	printable := false
+	// traverse nozzles
+	for _, row := range h.Rows {
+		for _, nozzle := range row.Nozzles {
+
+			// check available nozzles
+			for _, dot := range pf.DotsInRow(py) {
+				dotx, doty := ToPrintheadPosition(dot.PositionX, dot.PositionY)
+				if math.Abs(float64(nozzle.X-dotx*printheads.MM)) < float64(h.RowOffset) &&
+					math.Abs(float64(nozzle.Y-doty*printheads.MM)) < float64(h.RowOffset) {
+					if (dot.Base.Name == "A" && row.Index == 0) ||
+						(dot.Base.Name == "C" && row.Index == 1) ||
+						(dot.Base.Name == "G" && row.Index == 2) ||
+						(dot.Base.Name == "T" && row.Index == 3) {
+						dot.Printed = true
+						img.Set(dot.PositionX, dot.PositionY, dot.Base.Color)
+						fmt.Println(dot.Base.Name, nozzle, " || ", dot, " >> ", dotx, doty)
+						data[nozzle.Index] = int(dot.Base.Color.A)
+						printable = true
+					}
+				}
+			}
+
+		}
+	}
+	if printable {
+		fileName := fmt.Sprintf("output/%02d.png", *imageIndex)
+		outputFile, _ := os.Create(fileName)
+		png.Encode(outputFile, img)
+		outputFile.Close()
+		*imageIndex = *imageIndex + 1
+	}
+	return data
 }
