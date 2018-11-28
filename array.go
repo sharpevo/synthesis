3c6423202c3a0aa0538b10e7b649618fec6d27f9
yangwu <yang.wu@igenetech.com>
2018-11-28 10:42:51 +0800

    Initial commit

---
+++ array.go
@@ -0,0 +1,117 @@
+package printhead
+
+import ()
+
+type Array struct {
+	Nozzles     []*Nozzle
+	SightBottom *Nozzle
+	SightTop    *Nozzle
+}
+
+func NewArray(
+	nozzles []*Nozzle,
+) *Array {
+	a := &Array{
+		Nozzles: nozzles,
+	}
+	a.SightTop, a.SightBottom = a.sights()
+	return a
+}
+
+func (a *Array) sights() (top *Nozzle, bottom *Nozzle) {
+	// assume vertical layout and aligned
+	// and the distance is integer multiple of 4, i.e. 25.4/150
+	for _, nozzle := range a.Nozzles {
+		if nozzle.Index == 0 {
+			if bottom == nil {
+				bottom = nozzle
+			} else {
+				// bottom left nozzle of the bottom right printhead
+				if nozzle.Pos.Y < bottom.Pos.Y {
+					bottom = nozzle
+				}
+			}
+		}
+		if nozzle.Index == 3 {
+			if top == nil {
+				top = nozzle
+			} else {
+				// top left nozzle of the top right printhead
+				if nozzle.Pos.Y > top.Pos.Y {
+					top = nozzle
+				}
+			}
+		}
+	}
+	return top, bottom
+}
+
+func (a *Array) MoveTop(posx int, posy int) {
+	deltax := posx - a.SightTop.Pos.X
+	deltay := posy - a.SightTop.Pos.Y
+	for _, n := range a.Nozzles {
+		n.Pos.X = n.Pos.X + deltax
+		n.Pos.Y = n.Pos.Y + deltay
+	}
+}
+
+func (a *Array) MoveBottom(posx int, posy int) {
+	deltax := posx - a.SightBottom.Pos.X
+	deltay := posy - a.SightBottom.Pos.Y
+	for _, n := range a.Nozzles {
+		n.Pos.X = n.Pos.X + deltax
+		n.Pos.Y = n.Pos.Y + deltay
+	}
+}
+
+func (a *Array) MoveBottomRow(rowIndex, posx int, posy int) {
+	var deltax, deltay int
+	switch rowIndex {
+	case 0:
+		deltax = posx - a.SightBottom.Pos.X
+		deltay = posy - a.SightBottom.Pos.Y
+	case 1: // 3rd
+		deltax = posx - (a.SightBottom.Pos.X + 1)
+		deltay = posy - (a.SightBottom.Pos.Y + 279)
+	case 2: // 2nd
+		deltax = posx - (a.SightBottom.Pos.X + 2)
+		deltay = posy - (a.SightBottom.Pos.Y + 13)
+	case 3:
+		deltax = posx - (a.SightBottom.Pos.X + 3)
+		deltay = posy - (a.SightBottom.Pos.Y + 292)
+	}
+	for _, n := range a.Nozzles {
+		n.Pos.X = n.Pos.X + deltax
+		n.Pos.Y = n.Pos.Y + deltay
+	}
+}
+
+func (a *Array) MoveTopRow(rowIndex, posx int, posy int) {
+	var deltax, deltay int
+	switch rowIndex {
+	case 0:
+		deltax = posx - (a.SightTop.Pos.X - 3)
+		deltay = posy - (a.SightTop.Pos.Y - 292)
+	case 1: // 3rd
+		deltax = posx - (a.SightTop.Pos.X - 2)
+		deltay = posy - (a.SightTop.Pos.Y - 13)
+	case 2: // 2nd
+		deltax = posx - (a.SightTop.Pos.X - 1)
+		deltay = posy - (a.SightTop.Pos.Y - 279)
+	case 3:
+		deltax = posx - a.SightTop.Pos.X
+		deltay = posy - a.SightTop.Pos.Y
+	}
+	for _, n := range a.Nozzles {
+		n.Pos.X = n.Pos.X + deltax
+		n.Pos.Y = n.Pos.Y + deltay
+	}
+}
+
+func (a *Array) Top() int {
+	return a.SightTop.Pos.Y
+}
+
+func (a *Array) Bottom() int {
+	return a.SightBottom.Pos.Y
+}
---
+++ printhead.go
@@ -0,0 +1,59 @@
+package printhead
+
+import (
+	"posam/util/geometry"
+	"posam/util/reagent"
+)
+
+type Printhead struct {
+	DevicePath string
+	Reagents   []*reagent.Reagent
+}
+
+func NewPrinthead(
+	devicePath string,
+	reagents []*reagent.Reagent,
+) *Printhead {
+	p := &Printhead{
+		DevicePath: devicePath,
+		Reagents:   reagents,
+	}
+	return p
+}
+
+func (p *Printhead) MakeNozzles(
+	posx int,
+	posy int,
+) []*Nozzle {
+	nozzles := []*Nozzle{}
+	for index := 0; index < 1280; index++ {
+		nozzle, _ := NewNozzle(index)
+		nozzle.Reagent = p.Reagents[nozzle.RowIndex]
+		nozzle.Printhead = p
+		switch nozzle.RowIndex {
+		case 0:
+			nozzle.Pos = geometry.NewPosition(
+				posx+nozzle.Index,
+				posy,
+			)
+		case 1:
+			nozzle.Pos = geometry.NewPosition(
+				posx+nozzle.Index,
+				posy+279,
+			)
+		case 2:
+			nozzle.Pos = geometry.NewPosition(
+				posx+nozzle.Index,
+				posy+13,
+			)
+		case 3:
+			nozzle.Pos = geometry.NewPosition(
+				posx+nozzle.Index,
+				//posy+305,
+				posy+292,
+			)
+		}
+		nozzles = append(nozzles, nozzle)
+	}
+	return nozzles
+}
