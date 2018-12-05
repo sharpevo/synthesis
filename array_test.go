09ef68e5f5f8967e52b6b88388b4eacc32b58f65
yangwu <yang.wu@igenetech.com>
2018-12-05 09:46:22 +0800

    ADd TestMoveRowDownward TestMoveRowUpward

--- array_test.go
+++ array_test.go
@@ -1,7 +1,7 @@
 package printhead_test
 
 import (
-	"fmt"
+	//"fmt"
 	"posam/util/geometry"
 	"posam/util/printhead"
 	"posam/util/reagent"
@@ -17,7 +17,6 @@ func RoundByStep(
 ) int {
 	delta := geometry.Unit(p1 - p0)
 	rem := delta % step
-	fmt.Println(">", delta)
 	if rem > step/2 { // not include half of step
 		delta += step - rem
 	} else {
@@ -26,14 +25,7 @@ func RoundByStep(
 	return delta
 }
 
-func TestRoundByStep(t *testing.T) {
-	delta1 := RoundByStep(20.0, 65.0, 4)
-	fmt.Println(delta1)
-	delta2 := RoundByStep(20.0, 65.0, 2)
-	fmt.Println(delta2)
-}
-
-func TestNewArray(t *testing.T) {
+func newArray() *printhead.Array {
 	p0 := printhead.NewPrinthead(
 		"p0 path",
 		[]*reagent.Reagent{
@@ -59,11 +51,30 @@ func TestNewArray(t *testing.T) {
 	)
 	p1x, p1y := 35.0, 65.0
 	p1xUnit := geometry.Unit(p1x)
-	deltay := RoundByStep(p0y, p1y, 4)
+	deltay := RoundByStep(p0y, p1y, 4) // 1064
 	nozzles1 := p1.MakeNozzles(p1xUnit, p0yUnit+deltay)
 	array := printhead.NewArray(
 		append(nozzles0, nozzles1...),
 	)
+	return array
+}
+
+func TestRoundByStep(t *testing.T) {
+	var delta, expected int
+	delta = RoundByStep(20.0, 65.0, 4) // 1063
+	expected = 1064
+	if delta != expected {
+		t.Errorf(tmpl, expected, delta)
+	}
+	delta = RoundByStep(20.0, 65.0, 2)
+	expected = 1062
+	if delta != expected {
+		t.Errorf(tmpl, expected, delta)
+	}
+}
+
+func TestNewArray(t *testing.T) {
+	array := newArray()
 	expectedTop := geometry.NewPosition(830, 1828)
 	if !array.SightTop.Pos.Equal(expectedTop) {
 		t.Errorf(
@@ -73,6 +84,48 @@ func TestNewArray(t *testing.T) {
 		)
 	}
 	expectedBottom := geometry.NewPosition(827, 472)
+	if !array.SightBottom.Pos.Equal(expectedBottom) {
+		t.Errorf(
+			tmpl,
+			expectedBottom,
+			*array.SightBottom.Pos,
+		)
+	}
+}
+
+func TestMoveRowUpward(t *testing.T) {
+	array := newArray()
+	array.MoveTopRow(0, 10, 20)
+	expectedTop := geometry.NewPosition(13, 312) // 10 + 3, 20 + 292
+	if !array.SightTop.Pos.Equal(expectedTop) {
+		t.Errorf(
+			tmpl,
+			*expectedTop,
+			*array.SightTop.Pos,
+		)
+	}
+	expectedBottom := geometry.NewPosition(10, -1044) // 10, 312 - (1064 + 292)
+	if !array.SightBottom.Pos.Equal(expectedBottom) {
+		t.Errorf(
+			tmpl,
+			expectedBottom,
+			*array.SightBottom.Pos,
+		)
+	}
+}
+
+func TestMoveRowDownward(t *testing.T) {
+	array := newArray()
+	array.MoveBottomRow(0, 10, 20)
+	expectedTop := geometry.NewPosition(13, 1376) // 10 + 3, 20 + 1064 + 292
+	if !array.SightTop.Pos.Equal(expectedTop) {
+		t.Errorf(
+			tmpl,
+			*expectedTop,
+			*array.SightTop.Pos,
+		)
+	}
+	expectedBottom := geometry.NewPosition(10, 20)
 	if !array.SightBottom.Pos.Equal(expectedBottom) {
 		t.Errorf(
 			tmpl,
