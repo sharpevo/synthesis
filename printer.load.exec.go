281c089a258771ed36255636e2d1ebc3e3b0a2b1
yangwu <yang.wu@igenetech.com>
2019-01-21 17:33:15 +0800

    Add DEBUG for testing

--- printer.load.exec.go
+++ printer.load.exec.go
@@ -3,7 +3,12 @@ package instruction
 import (
 	"fmt"
 	"posam/dao"
+	//"posam/interpreter"
 )
+
+//var DEBUG = true
+
+var DEBUG = false
 
 func init() {
 	dao.InstructionMap.Set("LOADEXEC", InstructionPrinterLoadExec{})
@@ -30,13 +35,18 @@ func (i *InstructionPrinterLoadExec) Execute(args ...string) (resp interface{}, 
 			"invalid cycle index %v (%v)", cycleIndex, bin.CycleCount)
 	}
 	formations := bin.Formations[cycleIndex]
+	formationCount := len(formations)
+	if formationCount == 0 {
+		// happens when activator is checked but no activator reagent assigned
+		return fmt.Sprintf("no group in the cycle %v", cycleIndex), nil
+	}
 	groupIndex, err := i.ParseIndex(args[2])
 	if err != nil {
 		return resp, err
 	}
-	if groupIndex > len(formations)-1 || groupIndex < 0 {
+	if groupIndex > formationCount-1 || groupIndex < 0 {
 		return resp, fmt.Errorf(
-			"invalid cycle index %v (%v)", cycleIndex, bin.CycleCount)
+			"invalid group index %v (%v)", groupIndex, formationCount)
 	}
 
 	moveArgs := formations[groupIndex].Move
@@ -48,46 +58,53 @@ func (i *InstructionPrinterLoadExec) Execute(args ...string) (resp interface{}, 
 		moveArgs.MotorConf.Speed,
 		moveArgs.MotorConf.Accel,
 	)
-	moveIns := InstructionTMLMoveAbs{}
-	resp, err = moveIns.Execute(
-		moveArgs.MotorConf.Path,
-		moveArgs.PositionX,
-		moveArgs.PositionY,
-		moveArgs.MotorConf.Speed,
-		moveArgs.MotorConf.Accel,
-	)
-	if err != nil {
-		return resp, err
-	}
-	lineBuffers := ""
-	for _, printArgs := range formations[groupIndex].Prints {
-		fmt.Println(
-			"print",
-			printArgs.PrintheadConf.Path,
-			printArgs.PrintheadConf.BitsPerPixel,
-			printArgs.PrintheadConf.Width,
-			printArgs.PrintheadConf.BufferSize,
-			printArgs.LineBuffer,
-		)
-		lineBuffers += " | " + printArgs.LineBuffer
-		printIns := InstructionPrinterHeadPrintData{}
-		resp, err = printIns.Execute(
-			printArgs.PrintheadConf.Path,
-			printArgs.PrintheadConf.BitsPerPixel,
-			printArgs.PrintheadConf.Width,
-			printArgs.PrintheadConf.BufferSize,
-			printArgs.LineBuffer,
+	if !DEBUG {
+		moveIns := InstructionTMLMoveAbs{}
+		//moveIns.Env = interpreter.NewStack(i.Env)
+		moveIns.Env = NewStack(i.Env)
+		resp, err = moveIns.Execute(
+			moveArgs.MotorConf.Path,
+			moveArgs.PositionX,
+			moveArgs.PositionY,
+			moveArgs.MotorConf.Speed,
+			moveArgs.MotorConf.Accel,
 		)
 		if err != nil {
 			return resp, err
 		}
 	}
+
+	fmt.Println(
+		"print",
+		formations[groupIndex].Print.PrintheadConf.Path,
+		formations[groupIndex].Print.PrintheadConf.BitsPerPixel,
+		formations[groupIndex].Print.PrintheadConf.Width,
+		formations[groupIndex].Print.PrintheadConf.BufferSize,
+		formations[groupIndex].Print.LineBuffers,
+	)
+	if !DEBUG {
+		printIns := InstructionPrinterHeadPrintData{}
+		//printIns.Env = interpreter.NewStack(i.Env)
+		printIns.Env = NewStack(i.Env)
+		resp, err = printIns.Execute(
+			formations[groupIndex].Print.PrintheadConf.Path,
+			formations[groupIndex].Print.PrintheadConf.BitsPerPixel,
+			formations[groupIndex].Print.PrintheadConf.Width,
+			formations[groupIndex].Print.PrintheadConf.BufferSize,
+			formations[groupIndex].Print.LineBuffers[0],
+			formations[groupIndex].Print.LineBuffers[1],
+		)
+		if err != nil {
+			return resp, err
+		}
+	}
+
 	return fmt.Sprintf(
 		"cycle %v group %v: move (%v, %v), print %v",
 		cycleIndex,
 		groupIndex,
 		moveArgs.PositionX,
 		moveArgs.PositionY,
-		lineBuffers,
+		formations[groupIndex].Print.LineBuffers,
 	), err
 }
