5f9ea468f60735aafa6a6436f113be2470dd733a
yangwu <yang.wu@igenetech.com>
2019-02-14 15:49:49 +0800

    Move config functions to standalone package #459

--- util.go
+++ util.go
@@ -2,7 +2,6 @@ package util
 
 import (
 	"fmt"
-	"github.com/spf13/viper"
 	"log"
 	"os"
 	"runtime/debug"
@@ -29,65 +28,4 @@ func Go(function func()) {
 		}()
 		function()
 	}()
-}
-
-const CONFIG_FILE = "config.yml"
-
-var configFileLoaded = false
-
-func LoadConfig() {
-	if configFileLoaded {
-		return
-	}
-	fmt.Println("loading config...")
-	f, err := os.Open(CONFIG_FILE)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	viper.SetConfigType("yaml")
-	if err := viper.ReadConfig(f); err != nil {
-		fmt.Println(err)
-	}
-	f.Close()
-	viper.SetConfigFile(CONFIG_FILE)
-	configFileLoaded = true
-}
-
-func GetBool(key string) bool {
-	LoadConfig()
-	value := viper.GetBool(key)
-	if value {
-		fmt.Printf("ENABLE: %s\n", key)
-	}
-	return value
-}
-
-func Set(key string, value interface{}) error {
-	LoadConfig()
-	viper.Set(key, value)
-	err := viper.WriteConfig()
-	if err != nil {
-		fmt.Println(err)
-	}
-	return err
-}
-
-func GetString(key string) string {
-	LoadConfig()
-	return viper.GetString(key)
-}
-
-func GetInt(key string) int {
-	LoadConfig()
-	value := viper.GetInt(key)
-	fmt.Printf("ENABLE: %s = %v\n", key, value)
-	return value
-}
-
-func GetFloat(key string) float64 {
-	LoadConfig()
-	value := viper.GetFloat64(key)
-	fmt.Printf("ENABLE: %s = %v\n", key, value)
-
 }
