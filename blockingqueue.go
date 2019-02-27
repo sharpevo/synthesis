8dc8859b0aa9a1d685aec8ec22c9eeed6724c62c
yangwu <yang.wu@igenetech.com>
2019-02-27 14:46:14 +0800

    Add Next functions

--- blockingqueue.go
+++ blockingqueue.go
@@ -3,22 +3,26 @@ package blockingqueue
 import (
 	"conditionvariable"
 	"fmt"
+	"log"
+	"runtime/debug"
 	"sync"
 )
 
 type BlockingQueue struct {
-	sync.Mutex
+	lock          sync.Mutex
 	itemList      []interface{}
+	Cursor        chan int
 	pushCondition conditionvariable.ChannelCondition
 	termCondition conditionvariable.ChannelCondition
 }
 
 func NewBlockingQueue() *BlockingQueue {
-	return &BlockingQueue{
-		itemList:      []interface{}{},
-		pushCondition: conditionvariable.NewChannelCondition(),
-		termCondition: conditionvariable.NewChannelCondition(),
-	}
+	b := &BlockingQueue{}
+	b.itemList = []interface{}{}
+	b.Cursor = make(chan int)
+	b.pushCondition = conditionvariable.NewChannelCondition()
+	b.termCondition = conditionvariable.NewChannelCondition()
+	return b
 }
 
 func (b *BlockingQueue) Reset() {
@@ -65,10 +69,29 @@ func (b *BlockingQueue) Length() int {
 func (b *BlockingQueue) Get(index int) (interface{}, error) {
 	b.Lock()
 	defer b.Unlock()
-	if index < 0 || index > len(b.itemList) {
+	if index < 0 || index >= len(b.itemList) {
 		return nil, fmt.Errorf("invalid index")
 	}
-	return b.itemList[index], nil
+	item := b.itemList[index]
+	return item, nil
+}
+
+func (b *BlockingQueue) GetLockless(index int) (interface{}, error) {
+	defer func() {
+		if err := recover(); err != nil {
+			msg := fmt.Sprintf(
+				"Panic: %s\n%s",
+				err, debug.Stack(),
+			)
+			log.Println("---------- panic ----------")
+			log.Fatal(msg)
+		}
+	}()
+	if index < 0 || index >= len(b.itemList) {
+		return nil, fmt.Errorf("invalid index")
+	}
+	item := b.itemList[index]
+	return item, nil
 }
 
 func (b *BlockingQueue) Append(item interface{}) {
@@ -93,4 +116,70 @@ func (b *BlockingQueue) Iter() <-chan Item {
 		}
 	}()
 	return itemc
+}
+
+func (b *BlockingQueue) Next() <-chan Item {
+	itemc := make(chan Item)
+	go func() {
+		defer close(itemc)
+		b.Lock()
+		defer b.Unlock()
+		var cursor int
+		for cursor < len(b.itemList) {
+			cursor = <-b.Cursor
+			fmt.Println("CURSOR", cursor, len(b.itemList))
+			fmt.Printf("CURSOR queue: %#v\n", b)
+			fmt.Printf("CURSOR list: %#v\n", b.itemList)
+			if cursor > len(b.itemList)-1 {
+				break
+			}
+			if cursor == -1 {
+				break
+			}
+			var item interface{}
+			item = b.itemList[cursor]
+			fmt.Printf("BLOCKINGQUEUE: %#v\n", item)
+			itemc <- Item{cursor, item}
+		}
+	}()
+	return itemc
+}
+
+func (b *BlockingQueue) ItemList() []interface{} {
+	b.Lock()
+	defer b.Unlock()
+	return b.itemList
+}
+
+func (b *BlockingQueue) NextLockless() <-chan Item {
+	itemc := make(chan Item)
+	go func() {
+		defer close(itemc)
+		var cursor int
+		for cursor < len(b.itemList) {
+			cursor = <-b.Cursor
+			fmt.Println("CURSOR", cursor, len(b.itemList))
+			fmt.Printf("CURSOR queue: %#v\n", b)
+			fmt.Printf("CURSOR list: %#v\n", b.itemList)
+			if cursor > len(b.itemList)-1 {
+				break
+			}
+			if cursor == -1 {
+				break
+			}
+			var item interface{}
+			item = b.itemList[cursor]
+			fmt.Printf("BLOCKINGQUEUE: %#v\n", item)
+			itemc <- Item{cursor, item}
+		}
+	}()
+	return itemc
+}
+
+func (b *BlockingQueue) Lock() {
+	b.lock.Lock()
+}
+
+func (b *BlockingQueue) Unlock() {
+	b.lock.Unlock()
 }
