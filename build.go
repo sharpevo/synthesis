a24895c646df18e9a986b0e3e7b0714a0d83bbe4
yangwu <yang.wu@igenetech.com>
2019-03-07 15:36:12 +0800

    Make it work

--- build.go
+++ build.go
@@ -1,166 +1,131 @@
 package formation
 
-func build(
+import (
+	"fmt"
+	"image"
+	"image/png"
+	"os"
+	"posam/util/geometry"
+	"posam/util/log"
+	"posam/util/reagent"
+	"strconv"
+	"strings"
+)
+
+const (
+	BINFILE  = "test.bin"
+	IMAGABLE = true
+)
+
+func (b *Bin) Build(
 	step int,
-	cycleCount int,
-	printheadArray *printhead.Array,
-	subs *substrate.Substrate,
-	motorPath string,
-	motorSpeed string,
-	motorAccel string,
-	printhead0Path string,
-	buildProgressbar *widgets.QProgressBar,
-) {
-	bin := formation.NewBin(
-		cycleCount,
-		formation.NewMotorConf(
-			motorPath,
-			motorSpeed,
-			motorAccel,
-		),
-		formation.NewPrintheadConf(
-			printhead0Path,
-			"1",
-			"2560",
-			"320",
-		),
-	)
-	fmt.Println("create bin", bin)
-	imageIndex := 0
-
+	img *image.RGBA,
+) (countc chan int) {
+	countc = make(chan int)
 	go func() {
-		for cycleIndex := 0; cycleIndex < cycleCount; cycleIndex++ {
-			img := image.NewRGBA(image.Rect(0, 0, subs.Width, subs.Height+1))
-			fmt.Println("cycle ", cycleIndex)
-			stripSum := subs.Strip()
+		stepCount := 0
+		for cycleIndex := 0; cycleIndex < b.CycleCount; cycleIndex++ {
+			img.Pix = make([]uint8, 4*b.Substrate.Width*(b.Substrate.Height+1))
+			log.V("cycle index", cycleIndex).Debug()
+			stripSum := b.Substrate.Strip()
 			for stripCount := 0; stripCount < stripSum; stripCount++ {
-
-				fmt.Println("strip ", stripCount)
+				log.V("strip count", stripCount).Debug()
 				posx := stripCount * 1280
-				posy := subs.Top()
-
+				posy := b.Substrate.Top()
 				rowIndex := 3
-
-				printheadArray.MoveBottomRow(rowIndex, posx, posy)
-				fmt.Printf("move row %v to %v %v\n", rowIndex, posx, posy)
-
-				for printheadArray.Top() >= subs.Bottom() {
-					dataMap, count := genData(cycleIndex, printheadArray, subs, img, &imageIndex)
+				b.PrintheadArray.MoveBottomRow(rowIndex, posx, posy)
+				log.Df("move row %d to %d %d\n", rowIndex, posx, posy)
+				for b.PrintheadArray.Top() >= b.Substrate.Bottom() {
+					dataMap, count := b.genData(
+						cycleIndex, img, &stepCount)
 					if count > 0 {
-						if DEBUGABLE {
-							fmt.Println("data downward #1", count, dataMap)
-						}
-						x, y := RawPos(
-							printheadArray.SightBottom.Pos.X,
-							printheadArray.SightBottom.Pos.Y,
-						)
-						bin.AddFormation(
-							cycleIndex, x, y, dataMap,
-						)
+						log.Vs(log.M{
+							"count":    count,
+							"data map": dataMap,
+						}).Debug("data downward #1")
+						x, y := b.RawPos()
+						b.AddFormation(cycleIndex, x, y, dataMap)
 					}
 					posy -= step
-					printheadArray.MoveBottomRow(rowIndex, posx, posy)
+					b.PrintheadArray.MoveBottomRow(rowIndex, posx, posy)
 				}
-
 				if step == 1 {
 					continue
 				}
-
-				posy = subs.Bottom()
-				// distance is integer multiple of 4
-				// so that move one row will match the others
+				posy = b.Substrate.Bottom()
 				rowIndex = 2
-				printheadArray.MoveTopRow(rowIndex, posx, posy)
-				fmt.Printf("move row %v to %v %v\n", rowIndex, posx, posy)
-
-				for printheadArray.Bottom() <= subs.Top() {
-					dataMap, count := genData(cycleIndex, printheadArray, subs, img, &imageIndex)
+				b.PrintheadArray.MoveTopRow(rowIndex, posx, posy)
+				log.Df("move row %d to %d %d\n", rowIndex, posx, posy)
+				for b.PrintheadArray.Bottom() <= b.Substrate.Top() {
+					dataMap, count := b.genData(
+						cycleIndex, img, &stepCount)
 					if count > 0 {
-						if DEBUGABLE {
-							fmt.Println("data upward #2", count, dataMap)
-						}
-						// use the bottom position
-						// sinc the offset is bottomed
-						x, y := RawPos(
-							printheadArray.SightBottom.Pos.X,
-							printheadArray.SightBottom.Pos.Y,
-						)
-						bin.AddFormation(
-							cycleIndex, x, y, dataMap,
-						)
+						log.Vs(log.M{
+							"count":    count,
+							"data map": dataMap,
+						}).Debug("data upward #2")
+						x, y := b.RawPos()
+						b.AddFormation(cycleIndex, x, y, dataMap)
 					}
 					posy += step
-					printheadArray.MoveTopRow(rowIndex, posx, posy)
+					b.PrintheadArray.MoveTopRow(rowIndex, posx, posy)
 				}
-
-				posy = subs.Top()
+				posy = b.Substrate.Top()
 				rowIndex = 1
-				printheadArray.MoveBottomRow(rowIndex, posx, posy)
-				fmt.Printf("move row %v to %v %v\n", rowIndex, posx, posy)
-
-				for printheadArray.Top() >= subs.Bottom() {
-					dataMap, count := genData(cycleIndex, printheadArray, subs, img, &imageIndex)
+				b.PrintheadArray.MoveBottomRow(rowIndex, posx, posy)
+				log.Df("move row %d to %d %d\n", rowIndex, posx, posy)
+				for b.PrintheadArray.Top() >= b.Substrate.Bottom() {
+					dataMap, count := b.genData(
+						cycleIndex, img, &stepCount)
 					if count > 0 {
-						if DEBUGABLE {
-							fmt.Println("data downward #3", count, dataMap)
-						}
-						x, y := RawPos(
-							printheadArray.SightBottom.Pos.X,
-							printheadArray.SightBottom.Pos.Y,
-						)
-						bin.AddFormation(
-							cycleIndex, x, y, dataMap,
-						)
+						log.Vs(log.M{
+							"count":    count,
+							"data map": dataMap,
+						}).Debug("data downward #3")
+						x, y := b.RawPos()
+						b.AddFormation(cycleIndex, x, y, dataMap)
 					}
 					posy -= step
-					printheadArray.MoveBottomRow(rowIndex, posx, posy)
+					b.PrintheadArray.MoveBottomRow(rowIndex, posx, posy)
 				}
-
-				posy = subs.Bottom()
+				posy = b.Substrate.Bottom()
 				rowIndex = 0
-				printheadArray.MoveTopRow(rowIndex, posx, posy)
-				fmt.Printf("move row %v to %v %v\n", rowIndex, posx, posy)
-
-				for printheadArray.Bottom() <= subs.Top() {
-					dataMap, count := genData(cycleIndex, printheadArray, subs, img, &imageIndex)
+				b.PrintheadArray.MoveTopRow(rowIndex, posx, posy)
+				log.Df("move row %d to %d %d\n", rowIndex, posx, posy)
+				for b.PrintheadArray.Bottom() <= b.Substrate.Top() {
+					dataMap, count := b.genData(
+						cycleIndex, img, &stepCount)
 					if count > 0 {
-						if DEBUGABLE {
-							fmt.Println("data upward #4", count, dataMap)
-						}
-						// use the bottom position
-						// sinc the offset is bottomed
-						x, y := RawPos(
-							printheadArray.SightBottom.Pos.X,
-							printheadArray.SightBottom.Pos.Y,
-						)
-						bin.AddFormation(
-							cycleIndex, x, y, dataMap,
-						)
+						log.Vs(log.M{
+							"count":    count,
+							"data map": dataMap,
+						}).Debug("data upward #4")
+						x, y := b.RawPos()
+						b.AddFormation(cycleIndex, x, y, dataMap)
 					}
 					posy += step
-					printheadArray.MoveTopRow(rowIndex, posx, posy)
+					b.PrintheadArray.MoveTopRow(rowIndex, posx, posy)
 				}
 			}
-			buildProgressbar.SetValue((cycleIndex + 1) * buildProgressbar.Maximum() / cycleCount)
+			countc <- cycleIndex + 1
 		}
-		err := bin.SaveToFile("test.bin")
+		err := b.SaveToFile(BINFILE)
 		if err != nil {
-			fmt.Println(err)
+			log.E(err.Error())
 		}
+		close(countc)
 	}()
-
+	return countc
 }
 
-func genData(
+func (b *Bin) genData(
 	cycleIndex int,
-	printheadArray *printhead.Array,
-	subs *substrate.Substrate,
 	img *image.RGBA,
-	imageIndex *int,
+	stepCount *int,
 ) ([]string, int) {
 	count := 0
-	dataSlice := make([][]string, printheadArray.PrintheadCount)
-	for _, nozzle := range printheadArray.Nozzles {
+	dataSlice := make([][]string, b.PrintheadArray.PrintheadCount)
+	for _, nozzle := range b.PrintheadArray.Nozzles {
 		if dataSlice[nozzle.Printhead.Index] == nil {
 			dataSlice[nozzle.Printhead.Index] = make([]string, 1280)
 		}
@@ -168,16 +133,17 @@ func genData(
 		if nozzle.Reagent.Equal(reagent.Nil) {
 			continue
 		}
-		//fmt.Println(nozzle.Pos.X, nozzle.Pos.Y, subs.Width, subs.Height)
-		if nozzle.Pos.Y >= subs.Height ||
+		//log.D(nozzle.Pos.X, nozzle.Pos.Y, b.Substrate.Width, b.Substrate.Height)
+		if nozzle.Pos.Y >= b.Substrate.Height ||
 			nozzle.Pos.Y < 0 ||
-			nozzle.Pos.X >= subs.Width ||
+			nozzle.Pos.X >= b.Substrate.Width ||
 			nozzle.Pos.X < 0 {
 			continue
 		}
-		spot := subs.Spots[nozzle.Pos.Y][nozzle.Pos.X]
+		spot := b.Substrate.Spots[nozzle.Pos.Y][nozzle.Pos.X]
 		if spot == nil || cycleIndex > len(spot.Reagents)-1 {
-			//fmt.Println("not enough reagents")
+			//log.Vs(log.M{"spot": spot, "cycle index": cycleIndex}).
+			//Error("not enough reagents")
 			continue
 		}
 		if spot != nil &&
@@ -185,75 +151,39 @@ func genData(
 			count += 1
 			dataSlice[nozzle.Printhead.Index][nozzle.Index] = "1"
 
-			if DEBUGABLE {
-				//fmt.Printf(" | printing ", nozzle.Reagent.Name, nozzle.Pos.X, nozzle.Pos.Y)
-			}
+			log.D("printing ", nozzle.Reagent.Name, nozzle.Pos.X, nozzle.Pos.Y)
 			if IMAGABLE {
-				img.Set(nozzle.Pos.X, subs.Height-nozzle.Pos.Y, nozzle.Reagent.Color)
+				img.Set(nozzle.Pos.X, b.Substrate.Height-nozzle.Pos.Y, nozzle.Reagent.Color)
 			}
 		}
 	}
 
-	output := make([]string, printheadArray.PrintheadCount)
+	output := make([]string, b.PrintheadArray.PrintheadCount)
 	if count > 0 {
 		for deviceIndex, dataBinSlice := range dataSlice {
 			dataHexSlice := make([]string, 160)
 			for i := 0; i < len(dataBinSlice); i += 8 {
-				value, _ := strconv.ParseInt(strings.Join(dataBinSlice[i:i+8], ""), 2, 64)
+				value, _ := strconv.ParseInt(
+					strings.Join(dataBinSlice[i:i+8], ""), 2, 64)
 				dataHexSlice = append(dataHexSlice, fmt.Sprintf("%02x", value))
 			}
 			output[deviceIndex] = strings.Join(dataHexSlice, "")
-			if DEBUGABLE {
-				fmt.Println("print device", deviceIndex)
-				fmt.Printf("data: %#v\n", dataBinSlice[:16])
-				fmt.Printf("linebuffer: %#v\n", output[deviceIndex][:8])
-			}
+			log.V("device index", deviceIndex).Debug()
+			log.V("dataBinSlice", dataBinSlice[:16]).Debug()
+			log.V("linebuffer", output[deviceIndex][:8]).Debug()
 		}
-
 		if IMAGABLE {
-			outputFile, _ := os.Create(fmt.Sprintf("output/%06d.%03d.png", *imageIndex, cycleIndex))
+			outputFile, _ := os.Create(
+				fmt.Sprintf("output/%06d.%03d.png", *stepCount, cycleIndex))
 			png.Encode(outputFile, img)
 			outputFile.Close()
-			*imageIndex = *imageIndex + 1
+			*stepCount = *stepCount + 1
 		}
 	}
 	return output, count
 }
 
-func RawPos(
-	posx int,
-	posy int,
-) (string, string) {
-	x := offsetX - geometry.Mm(posx)
-	y := offsetY - geometry.Mm(posy)
-	if DEBUGABLE {
-		fmt.Println("move to", x, y)
-	}
-	return fmt.Sprintf("%.6f", x), fmt.Sprintf("%.6f", y)
-}
-
-func MostLeftSpot(cycleIndex int, spots []*slide.Spot) *slide.Spot {
-	var target *slide.Spot
-	for _, spot := range spots {
-		if spot.Reagents[cycleIndex].Printed {
-			continue
-		}
-		if target == nil {
-			target = spot
-		} else {
-			if spot.Pos.AtLeft(target.Pos) {
-				target = spot
-			}
-		}
-	}
-	return target
-}
-
-func getStep(stepInput *widgets.QLineEdit) int {
-	result, err := strconv.Atoi(stepInput.Text())
-	if err != nil {
-		stepInput.SetText("1")
-		return 1
-	}
-	return result
+func (b *Bin) RawPos() (string, string) {
+	return fmt.Sprintf("%.6f", geometry.Raw(b.PrintheadArray.SightBottom.Pos.X, b.PrintheadArray.OffsetX())),
+		fmt.Sprintf("%.6f", geometry.Raw(b.PrintheadArray.SightBottom.Pos.Y, b.PrintheadArray.OffsetY()))
 }
