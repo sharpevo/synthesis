2dc72e548fa784983de0258971cff2b20383a8c7
yangwu <yang.wu@igenetech.com>
2019-03-20 14:23:34 +0800

    Add test cases

--- substrate_test.go
+++ substrate_test.go
@@ -1,159 +1,394 @@
 package substrate_test
 
 import (
-	"fmt"
 	"posam/util/substrate"
 	"testing"
 )
 
-func xTestNewSubstrate(t *testing.T) {
-	spots := []*substrate.Spot{}
-	for _ = range [50]int{} {
-		spots = append(spots, substrate.NewSpot())
+// prev{{{
+//func xTestNewSubstrate(t *testing.T) {
+//spots := []*substrate.Spot{}
+//for _ = range [50]int{} {
+//spots = append(spots, substrate.NewSpot())
+//}
+//s, err := substrate.NewSubstrate(
+//600/150,
+//3,
+//1, // 10 * 4 * 25.4 / 600
+//1, // 4 * 4 * 25.4 / 600
+//1, // 5 * 600 / 25.4
+//spots,
+//)
+//fmt.Println(err)
+//for y := range s.Spots {
+//for _, spot := range s.Spots[y] {
+////fmt.Printf("[%d %d", x, y)
+//if spot != nil {
+//fmt.Printf("S")
+//} else {
+//fmt.Printf("-")
+//}
+
+//}
+//fmt.Println()
+//}
+//fmt.Println()
+
+//for _, spot := range s.Spots[14] {
+//if spot != nil {
+//fmt.Printf("S")
+//} else {
+//fmt.Printf("-")
+//}
+//}
+////=== RUN   TestNewSubstrate
+////23 23 23 115
+////0 23
+////4 23
+////8 23
+////12 23
+////16 23
+////20 23
+////0 19
+////4 19
+////8 19
+////12 19
+////16 19
+////20 19
+////0 15
+////4 15
+////8 15
+////12 15
+////16 15
+////20 15
+////0 11
+////4 11
+////8 11
+////12 11
+////16 11
+////20 11
+////0 7
+////4 7
+////8 7
+////12 7
+////16 7
+////20 7
+////0 3
+////4 3
+////8 3
+////12 3
+////16 3
+////20 3
+////46 23
+////50 23
+////54 23
+////58 23
+////62 23
+////66 23
+////46 19
+////50 19
+////54 19
+////58 19
+////62 19
+////66 19
+////46 15
+////50 15
+////<nil>
+////-------------------------------------------------------------------------------------------------------------------
+////-------------------------------------------------------------------------------------------------------------------
+////S---S---S---S---S---S----------------------------------------------------------------------------------------------
+////-------------------------------------------------------------------------------------------------------------------
+////-------------------------------------------------------------------------------------------------------------------
+////-------------------------------------------------------------------------------------------------------------------
+////S---S---S---S---S---S----------------------------------------------------------------------------------------------
+////-------------------------------------------------------------------------------------------------------------------
+////-------------------------------------------------------------------------------------------------------------------
+////-------------------------------------------------------------------------------------------------------------------
+////S---S---S---S---S---S----------------------------------------------------------------------------------------------
+////-------------------------------------------------------------------------------------------------------------------
+////-------------------------------------------------------------------------------------------------------------------
+////-------------------------------------------------------------------------------------------------------------------
+////S---S---S---S---S---S-------------------------S---S----------------------------------------------------------------
+////-------------------------------------------------------------------------------------------------------------------
+////-------------------------------------------------------------------------------------------------------------------
+////-------------------------------------------------------------------------------------------------------------------
+////S---S---S---S---S---S-------------------------S---S---S---S---S---S------------------------------------------------
+////-------------------------------------------------------------------------------------------------------------------
+////-------------------------------------------------------------------------------------------------------------------
+////-------------------------------------------------------------------------------------------------------------------
+////S---S---S---S---S---S-------------------------S---S---S---S---S---S------------------------------------------------
+
+////S---S---S---S---S---S-------------------------S---S----------------------------------------------------------------
+//}
+
+//func xTestNewSubstrate2(t *testing.T) {
+//spots := []*substrate.Spot{}
+//for _ = range [40000]int{} {
+//spots = append(spots, substrate.NewSpot())
+//}
+//s, err := substrate.NewSubstrate(
+//600/150,
+//3,
+//20.0, // 10 * 4 * 25.4 / 600
+//50.0, // 4 * 4 * 25.4 / 600
+//2.0,  // 5 * 600 / 25.4
+//spots,
+//)
+//fmt.Println(err)
+//for y := range s.Spots {
+//for _, spot := range s.Spots[y] {
+////fmt.Printf("[%d %d", x, y)
+//if spot != nil {
+//fmt.Printf("S")
+//} else {
+//fmt.Printf("-")
+//}
+
+//}
+//fmt.Println()
+//}
+//fmt.Println()
+
+//for _, spot := range s.Spots[14] {
+//if spot != nil {
+//fmt.Printf("S")
+//} else {
+//fmt.Printf("-")
+//}
+//}
+//}
+// }}}
+
+func TestNewSubstrate(t *testing.T) {
+	spots10x10 := []*substrate.Spot{}
+	for _ = range [900]int{} {
+		spots10x10 = append(spots10x10, substrate.NewSpot())
 	}
-	s, err := substrate.NewSubstrate(
-		600/150,
-		3,
-		1, // 10 * 4 * 25.4 / 600
-		1, // 4 * 4 * 25.4 / 600
-		1, // 5 * 600 / 25.4
-		spots,
-	)
-	fmt.Println(err)
-	for y := range s.Spots {
-		for _, spot := range s.Spots[y] {
-			//fmt.Printf("[%d %d", x, y)
-			if spot != nil {
-				fmt.Printf("S")
-			} else {
-				fmt.Printf("-")
+	spots11x14 := []*substrate.Spot{}
+	for _ = range [1638]int{} {
+		spots11x14 = append(spots11x14, substrate.NewSpot())
+	}
+	spots4x21 := []*substrate.Spot{}
+	for _ = range [27000]int{} {
+		spots4x21 = append(spots4x21, substrate.NewSpot())
+	}
+	// cases{{{
+	type input struct {
+		slidecounth int
+		slidecountv int
+
+		slidewidth  float64
+		slideheight float64
+
+		slidespaceh float64
+		slidespacev float64
+
+		spots []*substrate.Spot
+
+		spotspace int
+		leftmost  int
+	}
+
+	type output struct {
+		SpotCount  int
+		SpotSpaceu int
+
+		MaxSpotsh int
+		MaxSpotsv int
+		Width     int
+		Height    int
+		LeftMostu int
+
+		SlideHeight  float64
+		SlideWidth   float64
+		SlideHeightu int
+		SlideWidthu  int
+		SlideNumh    int
+		SlideNumv    int
+		SlideSpacehu int
+		SlideSpacevu int
+
+		Top    int
+		Bottom int
+	}
+	cases := []struct {
+		title  string
+		input  input
+		output output
+	}{
+		{
+			"10x10",
+			input{
+				slidecounth: 3,
+				slidecountv: 3,
+				slidewidth:  1.524,
+				slideheight: 1.524,
+				slidespaceh: 1,
+				slidespacev: 1,
+				spots:       spots10x10,
+				spotspace:   4,
+				leftmost:    0,
+			},
+			output{
+				Top:          156,
+				Bottom:       0,
+				SpotCount:    900,
+				SpotSpaceu:   4,
+				MaxSpotsh:    156,
+				MaxSpotsv:    156,
+				Width:        157,
+				Height:       157,
+				LeftMostu:    0,
+				SlideHeight:  1.524,
+				SlideWidth:   1.524,
+				SlideHeightu: 36,
+				SlideWidthu:  36,
+				SlideNumh:    3,
+				SlideNumv:    3,
+				SlideSpacehu: 24,
+				SlideSpacevu: 24,
+			},
+		},
+		{
+			"11x14",
+			input{
+				slidecounth: 3,
+				slidecountv: 3,
+				slidewidth:  2.201333,
+				slideheight: 2.032,
+				slidespaceh: 1,
+				slidespacev: 1,
+				spots:       spots11x14,
+				spotspace:   4,
+				leftmost:    0,
+			},
+			output{
+				Top:          192,
+				Bottom:       0,
+				SpotCount:    1638,
+				SpotSpaceu:   4,
+				MaxSpotsh:    204,
+				MaxSpotsv:    192,
+				Width:        205,
+				Height:       193,
+				LeftMostu:    0,
+				SlideWidth:   2.201333,
+				SlideHeight:  2.032,
+				SlideWidthu:  52,
+				SlideHeightu: 48,
+				SlideNumh:    3,
+				SlideNumv:    3,
+				SlideSpacehu: 24,
+				SlideSpacevu: 24,
+			},
+		},
+		{
+			"4x21",
+			input{
+				slidecounth: 3,
+				slidecountv: 3,
+				slidewidth:  4,
+				slideheight: 21,
+				slidespaceh: 2,
+				slidespacev: 4,
+				spots:       spots4x21,
+				spotspace:   4,
+				leftmost:    1652,
+			},
+			output{
+				Top:          1678,
+				Bottom:       2,
+				SpotCount:    27000,
+				SpotSpaceu:   4,
+				MaxSpotsh:    379,
+				MaxSpotsv:    1678,
+				Width:        380,
+				Height:       1679,
+				LeftMostu:    1652,
+				SlideWidth:   4,
+				SlideHeight:  21,
+				SlideWidthu:  92,
+				SlideHeightu: 496,
+				SlideNumh:    3,
+				SlideNumv:    3,
+				SlideSpacehu: 44,
+				SlideSpacevu: 92,
+			},
+		},
+	} // }}}
+	for _, c := range cases {
+		t.Run(c.title, func(t *testing.T) {
+			//t.Parallel()
+			s, err := substrate.NewSubstrate(
+				c.input.slidecounth,
+				c.input.slidecountv,
+				c.input.slidewidth,
+				c.input.slideheight,
+				c.input.slidespaceh,
+				c.input.slidespacev,
+				c.input.spots,
+				c.input.spotspace,
+				c.input.leftmost,
+			)
+			if err != nil ||
+				s.Top() != c.output.Top ||
+				s.Bottom() != c.output.Bottom ||
+				s.SpotCount != c.output.SpotCount ||
+				s.SpotSpaceu != c.output.SpotSpaceu ||
+				s.MaxSpotsh != c.output.MaxSpotsh ||
+				s.MaxSpotsv != c.output.MaxSpotsv ||
+				s.Width != c.output.Width ||
+				s.Height != c.output.Height ||
+				s.LeftMostu != c.output.LeftMostu ||
+				s.SlideWidth != c.output.SlideWidth ||
+				s.SlideHeight != c.output.SlideHeight ||
+				s.SlideWidthu != c.output.SlideWidthu ||
+				s.SlideHeightu != c.output.SlideHeightu ||
+				s.SlideNumh != c.output.SlideNumh ||
+				s.SlideNumv != c.output.SlideNumv ||
+				s.SlideSpacehu != c.output.SlideSpacehu ||
+				s.SlideSpacevu != c.output.SlideSpacevu {
+				t.Error(
+					err,
+					"\nEXPECT:\n",
+					c.output.Top,
+					c.output.Bottom,
+					c.output.SpotCount,
+					c.output.SpotSpaceu,
+					c.output.MaxSpotsh,
+					c.output.MaxSpotsv,
+					c.output.Width,
+					c.output.Height,
+					c.output.LeftMostu,
+					c.output.SlideWidth,
+					c.output.SlideHeight,
+					c.output.SlideWidthu,
+					c.output.SlideHeightu,
+					c.output.SlideNumh,
+					c.output.SlideNumv,
+					c.output.SlideSpacehu,
+					c.output.SlideSpacevu,
+					"\nACTUAL:\n",
+					s.Top(),
+					s.Bottom(),
+					s.SpotCount,
+					s.SpotSpaceu,
+					s.MaxSpotsh,
+					s.MaxSpotsv,
+					s.Width,
+					s.Height,
+					s.LeftMostu,
+					s.SlideWidth,
+					s.SlideHeight,
+					s.SlideWidthu,
+					s.SlideHeightu,
+					s.SlideNumh,
+					s.SlideNumv,
+					s.SlideSpacehu,
+					s.SlideSpacevu,
+				)
 			}
 
-		}
-		fmt.Println()
-	}
-	fmt.Println()
-
-	for _, spot := range s.Spots[14] {
-		if spot != nil {
-			fmt.Printf("S")
-		} else {
-			fmt.Printf("-")
-		}
-	}
-	//=== RUN   TestNewSubstrate
-	//23 23 23 115
-	//0 23
-	//4 23
-	//8 23
-	//12 23
-	//16 23
-	//20 23
-	//0 19
-	//4 19
-	//8 19
-	//12 19
-	//16 19
-	//20 19
-	//0 15
-	//4 15
-	//8 15
-	//12 15
-	//16 15
-	//20 15
-	//0 11
-	//4 11
-	//8 11
-	//12 11
-	//16 11
-	//20 11
-	//0 7
-	//4 7
-	//8 7
-	//12 7
-	//16 7
-	//20 7
-	//0 3
-	//4 3
-	//8 3
-	//12 3
-	//16 3
-	//20 3
-	//46 23
-	//50 23
-	//54 23
-	//58 23
-	//62 23
-	//66 23
-	//46 19
-	//50 19
-	//54 19
-	//58 19
-	//62 19
-	//66 19
-	//46 15
-	//50 15
-	//<nil>
-	//-------------------------------------------------------------------------------------------------------------------
-	//-------------------------------------------------------------------------------------------------------------------
-	//S---S---S---S---S---S----------------------------------------------------------------------------------------------
-	//-------------------------------------------------------------------------------------------------------------------
-	//-------------------------------------------------------------------------------------------------------------------
-	//-------------------------------------------------------------------------------------------------------------------
-	//S---S---S---S---S---S----------------------------------------------------------------------------------------------
-	//-------------------------------------------------------------------------------------------------------------------
-	//-------------------------------------------------------------------------------------------------------------------
-	//-------------------------------------------------------------------------------------------------------------------
-	//S---S---S---S---S---S----------------------------------------------------------------------------------------------
-	//-------------------------------------------------------------------------------------------------------------------
-	//-------------------------------------------------------------------------------------------------------------------
-	//-------------------------------------------------------------------------------------------------------------------
-	//S---S---S---S---S---S-------------------------S---S----------------------------------------------------------------
-	//-------------------------------------------------------------------------------------------------------------------
-	//-------------------------------------------------------------------------------------------------------------------
-	//-------------------------------------------------------------------------------------------------------------------
-	//S---S---S---S---S---S-------------------------S---S---S---S---S---S------------------------------------------------
-	//-------------------------------------------------------------------------------------------------------------------
-	//-------------------------------------------------------------------------------------------------------------------
-	//-------------------------------------------------------------------------------------------------------------------
-	//S---S---S---S---S---S-------------------------S---S---S---S---S---S------------------------------------------------
-
-	//S---S---S---S---S---S-------------------------S---S----------------------------------------------------------------
-}
-
-func TestNewSubstrate2(t *testing.T) {
-	spots := []*substrate.Spot{}
-	for _ = range [40000]int{} {
-		spots = append(spots, substrate.NewSpot())
-	}
-	s, err := substrate.NewSubstrate(
-		600/150,
-		3,
-		20.0, // 10 * 4 * 25.4 / 600
-		50.0, // 4 * 4 * 25.4 / 600
-		2.0,  // 5 * 600 / 25.4
-		spots,
-	)
-	fmt.Println(err)
-	for y := range s.Spots {
-		for _, spot := range s.Spots[y] {
-			//fmt.Printf("[%d %d", x, y)
-			if spot != nil {
-				fmt.Printf("S")
-			} else {
-				fmt.Printf("-")
-			}
-
-		}
-		fmt.Println()
-	}
-	fmt.Println()
-
-	for _, spot := range s.Spots[14] {
-		if spot != nil {
-			fmt.Printf("S")
-		} else {
-			fmt.Printf("-")
-		}
+		})
 	}
 }
