f517173ee4b224ba972789aea1c4cd18f26187fe
yangwu <yang.wu@igenetech.com>
2019-03-20 17:27:32 +0800

    Add Xv function

--- log.go
+++ log.go
@@ -66,12 +66,20 @@ func T(args ...interface{}) {
 	log.Trace(S(args)...)
 }
 
+func Tv(fields M) {
+	log.WithFields(logrus.Fields(fields.raw())).Trace()
+}
+
 func Tf(format string, args ...interface{}) {
 	log.Tracef(format, S(args)...)
 }
 
 func D(args ...interface{}) {
 	log.Debug(S(args)...)
+}
+
+func Dv(fields M) {
+	log.WithFields(logrus.Fields(fields.raw())).Debug()
 }
 
 func Df(format string, args ...interface{}) {
@@ -82,12 +90,20 @@ func I(args ...interface{}) {
 	log.Info(S(args)...)
 }
 
+func Iv(fields M) {
+	log.WithFields(logrus.Fields(fields.raw())).Info()
+}
+
 func If(format string, args ...interface{}) {
 	log.Infof(format, S(args)...)
 }
 
 func W(args ...interface{}) {
 	log.Warn(S(args)...)
+}
+
+func Wv(fields M) {
+	log.WithFields(logrus.Fields(fields.raw())).Warn()
 }
 
 func Wf(format string, args ...interface{}) {
@@ -98,6 +114,10 @@ func E(args ...interface{}) {
 	log.Error(S(args)...)
 }
 
+func Ev(fields M) {
+	log.WithFields(logrus.Fields(fields.raw())).Error()
+}
+
 func Ef(format string, args ...interface{}) {
 	log.Errorf(format, S(args)...)
 }
@@ -106,12 +126,20 @@ func F(args ...interface{}) {
 	log.Fatal(S(args)...)
 }
 
+func Fv(fields M) {
+	log.WithFields(logrus.Fields(fields.raw())).Fatal()
+}
+
 func Ff(format string, args ...interface{}) {
 	log.Fatalf(format, S(args)...)
 }
 
 func P(args ...interface{}) {
 	log.Panic(S(args)...)
+}
+
+func Pv(fields M) {
+	log.WithFields(logrus.Fields(fields.raw())).Panic()
 }
 
 func Pf(format string, args ...interface{}) {
