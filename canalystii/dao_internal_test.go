5d1b13154def2385e8052b7ae848885a81e98543
yangwu <yang.wu@igenetech.com>
2019-05-08 10:59:49 +0800

    Add TestReadSystemRom

--- canalystii/dao_internal_test.go
+++ canalystii/dao_internal_test.go
@@ -944,3 +944,89 @@ func TestWriteSystemRom(t *testing.T) { // {{{
 		})
 	}
 } // }}}
+
+func TestReadSystemRom(t *testing.T) { // {{{
+	cases := []struct {
+		address int
+
+		message []byte
+		output  []byte
+		resp    []byte
+		err     error
+	}{
+		{
+			1,
+
+			[]byte{
+				SystemRomWriteUnit.Request().Function,
+				0, 1, 0, 0, 0, 0,
+			},
+			[]byte{1, 2, 3, 4, 5, 6, 7, 8},
+			[]byte{3, 4},
+			nil,
+		},
+		{
+			65536,
+
+			[]byte{
+				SystemRomWriteUnit.Request().Function,
+				0, 1, 0, 0, 0, 0,
+			},
+			[]byte{},
+			[]byte{},
+			fmt.Errorf("65536 overflows uint16"),
+		},
+		{
+			1,
+
+			[]byte{
+				SystemRomWriteUnit.Request().Function,
+				0, 1, 0, 0, 0, 0,
+			},
+			[]byte{},
+			[]byte{},
+			fmt.Errorf("some error"),
+		},
+	}
+	originSend1 := send1
+	defer func() { send1 = originSend1 }()
+	for i, c := range cases {
+		t.Run(fmt.Sprintf("%v", i), func(t *testing.T) {
+			d := &Dao{}
+			send1 = func(
+				d *Dao,
+				message []byte,
+				comResp []byte,
+			) ([]byte, error) {
+				if !reflect.DeepEqual(message, c.message) {
+					t.Errorf(
+						"\nEXPECT: %v\n GET: %v\n\n",
+						c.message,
+						message,
+					)
+				}
+				return c.output, c.err
+			}
+			resp, err := d.ReadSystemRom(c.address)
+			if err != nil && c.err == nil {
+				t.Fatal(err)
+			}
+			if err != nil && !strings.Contains(err.Error(), c.err.Error()) {
+				t.Errorf(
+					"\nEXPECT: %v\n GET: %v\n\n",
+					c.err.Error(),
+					err.Error(),
+				)
+			}
+			if err == nil {
+				if !reflect.DeepEqual(resp, c.resp) {
+					t.Errorf(
+						"\nEXPECT: %v\n GET: %v\n\n",
+						c.resp,
+						resp,
+					)
+				}
+			}
+		})
+	}
+} // }}}
