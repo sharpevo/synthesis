bf020b8696a3281c1b8efb09f71e422103b6ae10
yangwu <yang.wu@igenetech.com>
2019-05-23 13:56:28 +0800

    Use previous definition of GoString

--- igenetech.cs
+++ igenetech.cs
@@ -4,22 +4,21 @@ using System.Text; // Encoding
 
 public struct GoString
 {
-    public IntPtr p;
-    public int n;
+    public string val;
+    public int len;
     public static implicit operator GoString(string s)
     {
-        Console.WriteLine("----");
-        Console.WriteLine(s);
-        Console.WriteLine(s.Length);
-        return new GoString(){p = Marshal.StringToHGlobalAuto(s), n = s.Length};
+        return new GoString(){val=s, len=s.Length};
     }
     public static implicit operator string(GoString s){
-        Console.WriteLine("++++");
-        byte[] bytes = new byte[s.n];
-        Marshal.Copy((IntPtr)s.p, bytes, 0, s.n);
-        Console.WriteLine(s.p);
-        Console.WriteLine(s.n);
-        return Encoding.UTF8.GetString(bytes);
+        //Console.WriteLine("++++");
+        //byte[] bytes = new byte[s.n];
+        //Marshal.Copy((IntPtr)s.p, bytes, 0, s.n);
+        //Console.WriteLine(s.p);
+        //Console.WriteLine(s.n);
+        //return Encoding.UTF8.GetString(bytes);
+        return s.val;
+
     }
 }
 public struct GoSlice
