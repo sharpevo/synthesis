649043109c34f93cdf567a183f6c116c3506a47a
yangwu <yang.wu@igenetech.com>
2019-05-23 11:10:09 +0800

    Raw GoString definition
    not work across platform, comparing with the previous version.
    Failed to get full length of string, since the dll is x64 arch and the
    executable file built by mcs is 32bit. Still not work even build with
    mcs -platform:x64 *.cs.

--- igenetech.cs
+++ igenetech.cs
@@ -1,16 +1,26 @@
-using System;
+using System; // for IntPtr
+using System.Runtime.InteropServices; // for Mashal
+using System.Text; // Encoding
 
 public struct GoString
 {
-    public string Value { get; set; }
-    public int Length { get; set; }
-
+    public IntPtr p;
+    public int n;
     public static implicit operator GoString(string s)
     {
-        return new GoString() { Value = s, Length = s.Length };
+        Console.WriteLine("----");
+        Console.WriteLine(s);
+        Console.WriteLine(s.Length);
+        return new GoString(){p = Marshal.StringToHGlobalAuto(s), n = s.Length};
     }
-
-    public static implicit operator string(GoString s) => s.Value;
+    public static implicit operator string(GoString s){
+        Console.WriteLine("++++");
+        byte[] bytes = new byte[s.n];
+        Marshal.Copy((IntPtr)s.p, bytes, 0, s.n);
+        Console.WriteLine(s.p);
+        Console.WriteLine(s.n);
+        return Encoding.UTF8.GetString(bytes);
+    }
 }
 public struct GoSlice
 {
