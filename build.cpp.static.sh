860031bff910d367b861823d4882028338d7d0c4
yangwu <yang.wu@igenetech.com>
2019-05-24 09:17:23 +0800

    Add output file

--- build.cpp.static.sh
+++ build.cpp.static.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-x86_64-w64-mingw32-g++ -static-libgcc -static-libstdc++ -Wl,-Bstatic -lstdc++ -lpthread -Wl,-Bdynamic can.cpp
+x86_64-w64-mingw32-g++ -static-libgcc -static-libstdc++ -Wl,-Bstatic -lstdc++ -lpthread -Wl,-Bdynamic -o can.cpp.exe can.cpp
