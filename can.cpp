9b48ea96aae61063600bde7d6be353b469a42f1d
yangwu <yang.wu@igenetech.com>
2019-05-24 09:04:00 +0800

    Add humiture and new dao

--- can.cpp
+++ can.cpp
@@ -5,6 +5,12 @@
 //extern "C" {
 //}
 typedef int (__stdcall *f_funci)(char*, char**);
+typedef int (__stdcall *f_readhumiture)(double*, double*);
+typedef int (__stdcall *f_newdao)(char*, char*,char*,char*,char*,char*,char*,char*,char*,char*);
+
+char* conv(std::string input){
+    return const_cast<char*>(input.c_str());
+}
 
 int main(){
     //Test();
@@ -32,5 +38,42 @@ int main(){
     std::cout << "Test() returned: " << Test(c, &c) << std::endl;
     std::cout << "output: " << c << std::endl; // TODO: utf8
     std::cout <<"---- done"<< std::endl;
+
+
+    f_newdao NewDao = (f_newdao)GetProcAddress(a, "NewDao");
+    if (!NewDao) {
+        std::cout << "could not locate the function: "<< "NewDao" << std::endl;
+    }
+
+    f_readhumiture ReadHumiture = (f_readhumiture)GetProcAddress(a, "ReadHumiture");
+    if (!ReadHumiture) {
+        std::cout << "could not locate the function: "<< "ReadHumiture" << std::endl;
+    }
+
+    std::string devtype = "4";
+    std::string devindex = "0";
+    std::string devid = "0x00000001";
+    std::string canindex = "0";
+    std::string acccode = "0x00000000";
+    std::string accmask = "0xFFFFFFFF";
+    std::string filter = "0";
+    std::string timing0 = "0x00";
+    std::string timing1 = "0x1c";
+    std::string mode = "0";
+
+    int dao = NewDao(conv(devtype),conv(devindex),conv(devid), conv(canindex), conv(acccode), conv(accmask), conv(filter), conv(timing0), conv(timing1), conv(mode));
+    std::cout << "NewDao: " << dao  << std::endl;
+
+
+    //double* humi = 0;
+    //double* temp = 0;
+    double humi;
+    double temp;
+    int humiture = ReadHumiture(&humi, &temp);
+    std::cout << "ReadHumiture: " << humiture  << std::endl;
+    std::cout << "humiture: " << humi  << std::endl;
+    std::cout << "temperature: " << temp  << std::endl;
+
     return 0;
 }
+
