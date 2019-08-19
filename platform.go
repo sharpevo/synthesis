20904b2350581d272a560863162d9695c65d5911
yangwu <yang.wu@igenetech.com>
2019-08-19 17:20:07 +0800

    Add functions

--- platform.go
+++ platform.go
@@ -8,35 +8,42 @@ import (
 	"strings"
 )
 
+const (
+	// units
+	NM = 1
+	UM = 1e3
+	MM = 1e6
+)
+
 type Base struct {
 	Name  string
-	Color color.NRGBA
+	Color *color.NRGBA
 }
 
 var data uint8 = 0xff
 
 var BaseA = &Base{
 	"A",
-	color.NRGBA{0xff, 0x00, 0x00, data},
+	&color.NRGBA{0xff, 0x00, 0x00, data},
 }
 
 var BaseC = &Base{
 	"C",
-	color.NRGBA{0x00, 0xff, 0x00, data},
+	&color.NRGBA{0x00, 0xff, 0x00, data},
 }
 var BaseG = &Base{
 	"G",
-	color.NRGBA{0x00, 0x00, 0xff, data},
+	&color.NRGBA{0x00, 0x00, 0xff, data},
 }
 
 var BaseT = &Base{
 	"T",
-	color.NRGBA{0xff, 0x00, 0xff, data},
+	&color.NRGBA{0xff, 0x00, 0xff, data},
 }
 
 var BaseN = &Base{
 	"N",
-	color.NRGBA{0x00, 0x00, 0x00, 0x00},
+	&color.NRGBA{0x00, 0x00, 0x00, 0x00},
 }
 
 func NewBase(base string) *Base {
@@ -80,8 +87,23 @@ type Dot struct {
 type Platform struct {
 	Width  int
 	Height int
+	Minx   int
+	Maxx   int
+	Miny   int
+	Maxy   int
 	Dots   [][]*Dot
 }
+
+//func NewPlatform(width int, height int) *Platform {
+//platform := &Platform{}
+//platform.Width = width
+//platform.Height = height
+//platform.Dots = make([][]*Dot, width)
+//for i := range platform.Dots {
+//platform.Dots[i] = make([]*Dot, height)
+//}
+//return platform
+//}
 
 func NewPlatform(width int, height int) *Platform {
 	platform := &Platform{}
@@ -94,8 +116,60 @@ func NewPlatform(width int, height int) *Platform {
 	return platform
 }
 
+func (p *Platform) Top() int {
+	p.rectangle()
+	return p.Maxy
+}
+
+func (p *Platform) Bottom() int {
+	p.rectangle()
+	return p.Miny
+}
+
+func (p *Platform) Left() int {
+	p.rectangle()
+	return p.Minx
+}
+
+func (p *Platform) Right() int {
+	p.rectangle()
+	return p.Maxx
+}
+
+func (p *Platform) rectangle() {
+	if p.Minx == 0 && p.Maxx == 0 && p.Miny == 0 && p.Maxy == 0 {
+		inited := false
+		for _, row := range p.Dots {
+			for _, dot := range row {
+				if dot == nil {
+					continue
+				}
+				if !inited {
+					p.Minx = dot.PositionX
+					p.Maxx = p.Minx
+					p.Miny = dot.PositionY
+					p.Maxy = p.Miny
+					inited = true
+				}
+				if dot.PositionX < p.Minx {
+					p.Minx = dot.PositionX
+				}
+				if dot.PositionX > p.Maxx {
+					p.Maxx = dot.PositionX
+				}
+				if dot.PositionY < p.Miny {
+					p.Miny = dot.PositionY
+				}
+				if dot.PositionY > p.Maxy {
+					p.Maxy = dot.PositionY
+				}
+			}
+		}
+	}
+}
+
 func (p *Platform) AddBase(x int, y int, base *Base) {
-	fmt.Println(x-50, 50-y, base)
+	//fmt.Println(x-50, 50-y, base)
 
 	dot := &Dot{
 		Base:      base,
@@ -116,6 +190,7 @@ func (p *Platform) AddBlock(block *Block) {
 	}
 }
 
+// deprecated
 func (p *Platform) NextPosition() (int, int, error) {
 	for posy, row := range p.Dots {
 		for posx, dot := range row {
@@ -130,6 +205,267 @@ func (p *Platform) NextPosition() (int, int, error) {
 	}
 	return 0, 0, fmt.Errorf("all dots are printed")
 }
+
+func (p *Platform) NextDot() (int, int, *Dot, error) {
+	for y, row := range p.Dots {
+		for x, dot := range row {
+			if dot == nil {
+				continue
+			}
+			if !dot.Printed {
+				return x, y, dot, nil
+			}
+		}
+	}
+	return 0, 0, nil, fmt.Errorf("all dots are printed")
+}
+
+func (p *Platform) AvailableDots() []*Dot {
+	dots := []*Dot{}
+	for _, row := range p.Dots {
+		for _, dot := range row {
+			if dot == nil {
+				continue
+			}
+			if !dot.Printed {
+				dots = append(dots, dot)
+			}
+		}
+	}
+	return dots
+}
+
+func (p *Platform) NextDotVertical() (int, int, int, *Dot, error) {
+	var target *Dot
+	var targetX int
+	var targetY int
+	sum := 0
+	for y, row := range p.Dots {
+		for x, dot := range row {
+			if dot == nil {
+				continue
+			}
+			sum += 1
+			if !dot.Printed {
+				if target == nil {
+					target = dot
+					targetX = x
+					targetY = y
+				} else {
+					if dot.PositionX < target.PositionX {
+						target = dot
+						targetX = x
+						targetY = y
+					}
+				}
+			}
+		}
+	}
+	if target == nil {
+		return sum, targetX, targetY, nil, fmt.Errorf("all dots are printed")
+	}
+	return sum, targetX, targetY, target, nil
+}
+
+func (p *Platform) NextDotPositionY(direction int, posx int, posy int) int {
+	var target *Dot
+	for _, row := range p.Dots {
+		for _, dot := range row {
+			if dot == nil {
+				continue
+			}
+			if !dot.Printed {
+				//if dot.PositionY == position {
+				//continue
+				//}
+				//if target == nil {
+				//target = dot
+				//} else {
+				//if dot.PositionX < target.PositionX {
+				//target = dot
+				//}
+				//}
+				if direction < 0 { // downward
+					if dot.PositionY >= posy {
+						continue
+					}
+				}
+				if direction > 0 { // upward
+					if dot.PositionY <= posy || dot.PositionX < posx {
+						continue
+					}
+				}
+				if target == nil {
+					target = dot
+				} else {
+					if dot.PositionX < target.PositionX {
+						target = dot
+					}
+				}
+
+				//if target == nil {
+				//if direction < 0 { // downward
+				//if dot.PositionY < position {
+				//target = dot
+				//} else {
+				//continue
+				//}
+				//} else {
+				//if dot.PositionY > position {
+				//target = dot
+				//} else {
+				//continue
+				//}
+				//}
+				//} else {
+				//if dot.PositionX < target.PositionX {
+				//if direction < 0 { // upward
+				//if dot.PositionY < position {
+				//target = dot
+				//} else {
+				//continue
+				//}
+				//} else {
+				//if dot.PositionY > position {
+				//target = dot
+				//} else {
+				//continue
+				//}
+				//}
+				//}
+				//}
+			}
+		}
+	}
+	if target == nil {
+		return (50*MM + 1) * direction
+	}
+	fmt.Println("........NEXT STEP Y", posx, posy, target.Base.Name, target.PositionX, target.PositionY)
+	return target.PositionY
+}
+
+func (p *Platform) NextDotPositionX() int {
+	var target *Dot
+	for _, row := range p.Dots {
+		for _, dot := range row {
+			if dot == nil {
+				continue
+			}
+			if !dot.Printed {
+				if target == nil {
+					target = dot
+				} else {
+					if dot.PositionX < target.PositionX {
+						target = dot
+					}
+				}
+			}
+		}
+	}
+	if target == nil {
+		return 50*MM + 1
+	}
+	fmt.Println("........NEXT STEP X", target.Base.Name, target.PositionX, target.PositionY)
+	return target.PositionX
+}
+
+func (p *Platform) NextDotInColumn(x int) (int, int, *Dot) {
+	var target *Dot
+	var targetX int
+	var targetY int
+	for y, row := range p.Dots {
+		for x, dot := range row {
+			if dot == nil {
+				continue
+			}
+			if dot.PositionX == x {
+				if !dot.Printed {
+					if target == nil {
+						target = dot
+						targetX = x
+						targetY = y
+					} else {
+						if dot.PositionY > target.PositionY { // most top
+							target = dot
+							targetX = x
+							targetY = y
+						}
+					}
+				}
+			}
+		}
+	}
+	return targetX, targetY, target
+}
+
+func (p *Platform) PreviousDot(x int, y int) (int, int, *Dot) {
+	y--
+	if y < 0 {
+		return 0, 0, nil
+	}
+	return x, y, p.Dots[y][x]
+}
+
+func (p *Platform) PreviousDotInColumn(x int) *Dot {
+	var target *Dot
+	for _, row := range p.Dots {
+		for _, dot := range row {
+			if dot == nil {
+				continue
+			}
+			if dot.PositionX == x {
+				if !dot.Printed {
+					if target == nil {
+						target = dot
+					} else {
+						if dot.PositionY < target.PositionY { // most bottom
+							target = dot
+						}
+					}
+				}
+			}
+		}
+	}
+	return target
+}
+
+func (p *Platform) NextDotAfter(x int) *Dot {
+	var target *Dot
+	for _, row := range p.Dots {
+		for _, dot := range row {
+			if dot == nil {
+				continue
+			}
+			if dot.PositionX > x {
+				if target == nil {
+					target = dot
+				} else {
+					if dot.PositionX < target.PositionX { // most bottom
+						target = dot
+					}
+				}
+			}
+		}
+	}
+	return target
+}
+
+//func (p *Platform) DotsInRow(y int) []*Dot {
+//output := []*Dot{}
+//for _, column := range p.Dots {
+//dot := column[y]
+//if dot == nil {
+//continue
+//}
+////fmt.Println(dot.Base.Name, dot.Printed, dot.PositionX, dot.PositionY)
+//if !dot.Printed {
+//output = append(output, dot)
+//}
+//}
+////fmt.Printf("%#v\n", output)
+////fmt.Println(len(output))
+//return output
+//}
 
 func (p *Platform) DotsInRow(y int) []*Dot {
 	output := []*Dot{}
@@ -164,7 +500,7 @@ func ParsePlatform(pngPath string) (*Platform, error) {
 	for y := 0; y < height; y++ {
 		for x := 0; x < width; x++ {
 			c := img.At(x, y).(color.NRGBA)
-			if c == BaseN.Color {
+			if c == *BaseN.Color {
 				continue
 			}
 			platform.AddBase(x, y, ColorToBase(&c))
